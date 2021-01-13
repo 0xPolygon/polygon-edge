9bc4959665dbcc89bc0b7fd3dfadfdc873a2c2d6
Ferran <ferranbt@protonmail.com>
2021-01-13 20:42:52 +0100

    Add block and log filters

--- api/jsonrpc/filter/filter_manager.go
+++ api/jsonrpc/filter/filter_manager.go
@@ -2,18 +2,27 @@ package filter
 
 import (
 	"container/heap"
+	"encoding/json"
 	"fmt"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/0xPolygon/minimal/blockchain"
 	"github.com/0xPolygon/minimal/types"
 	"github.com/google/uuid"
+	"github.com/hashicorp/go-hclog"
 )
 
-type blockSubscription interface {
-	Watch() chan blockchain.Event
-	Close()
+type Log struct {
+	types.Log
+
+	BlockNumber uint64     `json:"blockNumber"`
+	TxHash      types.Hash `json:"transactionHash"`
+	TxIndex     uint       `json:"transactionIndex"`
+	BlockHash   types.Hash `json:"blockHash"`
+	LogIndex    uint       `json:"logIndex"`
+	Removed     bool       `json:"removed"`
 }
 
 type Filter struct {
@@ -24,7 +33,7 @@ type Filter struct {
 
 	// log cache
 	// TODO: Specify this log object here instead of types
-	logs []types.Log
+	logs []*Log
 
 	// log filter
 	logFilter *LogFilter
@@ -48,44 +57,81 @@ func (f *Filter) match() bool {
 	return false
 }
 
-// store is the interface with the blockchain required
-// by the filter manager
-type store interface {
-	GetBody(hash types.Hash) (types.Body, error)
+type subscription interface {
+	Watch() chan blockchain.Event
+	Close()
 }
 
+// blockchain is the interface with the blockchain required
+// by the filter manager
+type store interface {
+	// Header returns the current header of the chain (genesis if empty)
+	Header() *types.Header
+
+	// GetReceiptsByHash returns the receipts for a hash
+	GetReceiptsByHash(hash types.Hash) ([]*types.Receipt, error)
+
+	// Subscribe subscribes for chain head events
+	Subscribe() subscription
+}
+
+var defaultTimeout = 1 * time.Minute
+
 type FilterManager struct {
-	Sub     blockSubscription
+	logger hclog.Logger
+
+	store   store
 	closeCh chan struct{}
+
+	watcher chan blockchain.Event
 
 	filters map[string]*Filter
 	lock    sync.Mutex
 
 	updateCh chan struct{}
 	timer    timeHeapImpl
+	timeout  time.Duration
 
 	blockStream *blockStream
 }
 
-func (f *FilterManager) Run() {
-	f.closeCh = make(chan struct{})
-	f.updateCh = make(chan struct{})
+func NewFilterManager(logger hclog.Logger, store store) *FilterManager {
+	m := &FilterManager{
+		logger:      logger.Named("filter"),
+		store:       store,
+		closeCh:     make(chan struct{}),
+		filters:     map[string]*Filter{},
+		updateCh:    make(chan struct{}),
+		timer:       timeHeapImpl{},
+		blockStream: &blockStream{},
+		timeout:     defaultTimeout,
+	}
 
+	// start blockstream with the current header
+	header := store.Header()
+	m.blockStream.push(header.Hash)
+
+	// start the head watcher
+	m.watcher = store.Subscribe().Watch()
+
+	return m
+}
+
+func (f *FilterManager) Run() {
 	// watch for new events in the blockchain
-	evntCh := f.Sub.Watch()
 
 	var timeoutCh <-chan time.Time
 	for {
 		// check for the next filter to be removed
-		filter, timeout := f.nextTimeoutFilter()
+		filter := f.nextTimeoutFilter()
 		if filter == nil {
 			timeoutCh = nil
 		} else {
-			timeoutCh = time.After(timeout.Sub(time.Now()))
+			timeoutCh = time.After(filter.timestamp.Sub(time.Now()))
 		}
 
 		select {
-		case evnt := <-evntCh:
+		case evnt := <-f.watcher:
 			// new blockchain event
 			if err := f.dispatchEvent(evnt); err != nil {
 				fmt.Println(err)
@@ -102,14 +148,22 @@ func (f *FilterManager) Run() {
 
 		case <-f.closeCh:
 			// stop the filter manager
-			f.Sub.Close()
 			return
 		}
 	}
 }
 
-func (f *FilterManager) nextTimeoutFilter() (*Filter, time.Time) {
-	return nil, time.Time{}
+func (f *FilterManager) nextTimeoutFilter() *Filter {
+	f.lock.Lock()
+	if len(f.filters) == 0 {
+		f.lock.Unlock()
+		return nil
+	}
+
+	// pop the first item
+	item := f.timer[0]
+	f.lock.Unlock()
+	return item
 }
 
 func (f *FilterManager) dispatchEvent(evnt blockchain.Event) error {
@@ -121,32 +175,103 @@ func (f *FilterManager) dispatchEvent(evnt blockchain.Event) error {
 		f.blockStream.push(header.Hash)
 	}
 
-	// check if there is a match for the log filters
-	for _, i := range f.filters {
-		if i.isLogFilter() {
-			// check if any of the current logs is from a removed block
+	processBlock := func(h *types.Header, removed bool) error {
+		// get the logs from the transaction
+		receipts, err := f.store.GetReceiptsByHash(h.Hash)
+		if err != nil {
+			return err
 		}
+
+		for indx, receipt := range receipts {
+			// check the logs with the filters
+			for _, log := range receipt.Logs {
+				for _, f := range f.filters {
+					if f.isLogFilter() {
+						if f.logFilter.Match(log) {
+							nn := &Log{
+								Log:         *log,
+								BlockNumber: h.Number,
+								BlockHash:   h.Hash,
+								TxHash:      receipt.TxHash,
+								TxIndex:     uint(indx),
+								Removed:     removed,
+							}
+							f.logs = append(f.logs, nn)
+						}
+					}
+				}
+			}
+		}
+		return nil
+	}
+
+	// process old chain
+	for _, i := range evnt.OldChain {
+		processBlock(i, true)
+	}
+	// process new chain
+	for _, i := range evnt.NewChain {
+		processBlock(i, false)
 	}
 
 	return nil
 }
 
-func (f *FilterManager) GetFilterChanges() {
+func (f *FilterManager) Exists(id string) bool {
+	f.lock.Lock()
+	_, ok := f.filters[id]
+	f.lock.Unlock()
+	return ok
+}
 
+var errFilterDoesNotExists = fmt.Errorf("filter does not exists")
+
+func (f *FilterManager) GetFilterChanges(id string) (string, error) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+
+	item, ok := f.filters[id]
+	if !ok {
+		return "", errFilterDoesNotExists
+	}
+
+	if !item.isBlockFilter() {
+		// log filter
+		res, err := json.Marshal(item.logs)
+		if err != nil {
+			return "", err
+		}
+		return string(res), nil
+	}
+
+	updates, newHead := item.block.getUpdates()
+	item.block = newHead
+
+	res := fmt.Sprintf("[\"%s\"]", strings.Join(updates, "\",\""))
+
+	return res, nil
 }
 
 func (f *FilterManager) Uninstall(id string) error {
 	f.lock.Lock()
 
+	item, ok := f.filters[id]
+	if !ok {
+		return errFilterDoesNotExists
+	}
+
+	delete(f.filters, id)
+	heap.Remove(&f.timer, item.index)
+
 	f.lock.Unlock()
 	return nil
 }
 
-func (f *FilterManager) NewBlockFilter() (string, error) {
+func (f *FilterManager) NewBlockFilter() string {
 	return f.addFilter(nil)
 }
 
-func (f *FilterManager) NewLogFilter(logFilter *LogFilter) (string, error) {
+func (f *FilterManager) NewLogFilter(logFilter *LogFilter) string {
 	return f.addFilter(logFilter)
 }
 
@@ -157,7 +282,40 @@ type LogFilter struct {
 	Topics    [][]types.Hash
 }
 
-func (f *FilterManager) addFilter(logFilter *LogFilter) (string, error) {
+// Match returns whether the receipt includes topics for this filter
+func (l *LogFilter) Match(log *types.Log) bool {
+	// check addresses
+	if len(l.Addresses) > 0 {
+		match := false
+		for _, addr := range l.Addresses {
+			if addr == log.Address {
+				match = true
+			}
+		}
+		if !match {
+			return false
+		}
+	}
+	// check topics
+	if len(l.Topics) > len(log.Topics) {
+		return false
+	}
+	for i, sub := range l.Topics {
+		match := len(sub) == 0
+		for _, topic := range sub {
+			if log.Topics[i] == topic {
+				match = true
+				break
+			}
+		}
+		if !match {
+			return false
+		}
+	}
+	return true
+}
+
+func (f *FilterManager) addFilter(logFilter *LogFilter) string {
 	f.lock.Lock()
 
 	filter := &Filter{
@@ -174,6 +332,7 @@ func (f *FilterManager) addFilter(logFilter *LogFilter) (string, error) {
 	}
 
 	f.filters[filter.id] = filter
+	filter.timestamp = time.Now().Add(f.timeout)
 	heap.Push(&f.timer, filter)
 
 	f.lock.Unlock()
@@ -183,7 +342,7 @@ func (f *FilterManager) addFilter(logFilter *LogFilter) (string, error) {
 	default:
 	}
 
-	return filter.id, nil
+	return filter.id
 }
 
 func (f *FilterManager) Close() {
@@ -207,8 +366,6 @@ func (t timeHeapImpl) Swap(i, j int) {
 func (t *timeHeapImpl) Push(x interface{}) {
 	n := len(*t)
 	item := x.(*Filter)
-	// 1 minute timeouts
-	item.timestamp = time.Now().Add(1 * time.Minute)
 	item.index = n
 	*t = append(*t, item)
 }
@@ -240,7 +397,7 @@ func (b *blockStream) Head() *headElem {
 func (b *blockStream) push(newBlock types.Hash) {
 	b.lock.Lock()
 	newHead := &headElem{
-		hash: newBlock,
+		hash: newBlock.String(),
 	}
 	if b.head != nil {
 		b.head.next = newHead
@@ -250,12 +407,12 @@ func (b *blockStream) push(newBlock types.Hash) {
 }
 
 type headElem struct {
-	hash types.Hash
+	hash string
 	next *headElem
 }
 
-func (h *headElem) getUpdates() ([]types.Hash, *headElem) {
-	res := []types.Hash{}
+func (h *headElem) getUpdates() ([]string, *headElem) {
+	res := []string{}
 
 	cur := h
 	for {
--- api/jsonrpc/filter/filter_manager_test.go
+++ api/jsonrpc/filter/filter_manager_test.go
@@ -2,17 +2,89 @@ package filter
 
 import (
 	"testing"
+	"time"
 
+	"github.com/0xPolygon/minimal/blockchain"
 	"github.com/0xPolygon/minimal/types"
+	"github.com/hashicorp/go-hclog"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFilterManager(t *testing.T) {
+func TestFilterMatch(t *testing.T) {
 
 }
 
+func TestLogFilter(t *testing.T) {
+
+}
+
+func TestBlockFilter(t *testing.T) {
+	store := &mockStore{
+		header: &types.Header{
+			Hash: types.StringToHash("0"),
+		},
+	}
+
+	m := NewFilterManager(hclog.NewNullLogger(), store)
+	go m.Run()
+
+	// add block filter
+	id := m.addFilter(nil)
+
+	// emit two events
+	store.emitEvent(&blockchain.Event{
+		NewChain: []*types.Header{
+			{
+				Hash: types.StringToHash("1"),
+			},
+			{
+				Hash: types.StringToHash("2"),
+			},
+		},
+	})
+
+	store.emitEvent(&blockchain.Event{
+		NewChain: []*types.Header{
+			{
+				Hash: types.StringToHash("3"),
+			},
+		},
+	})
+
+	// we need to wait for the manager to process the data
+	time.Sleep(500 * time.Millisecond)
+
+	m.GetFilterChanges(id)
+
+	// emit one more event, it should not return the
+	// first three hashes
+	store.emitEvent(&blockchain.Event{
+		NewChain: []*types.Header{
+			{
+				Hash: types.StringToHash("4"),
+			},
+		},
+	})
+
+	time.Sleep(500 * time.Millisecond)
+
+	m.GetFilterChanges(id)
+}
+
 func TestTimeout(t *testing.T) {
-	// TODO
+	store := &mockStore{}
+
+	m := NewFilterManager(hclog.NewNullLogger(), store)
+	m.timeout = 2 * time.Second
+
+	go m.Run()
+
+	// add block filter
+	id := m.addFilter(nil)
+
+	assert.True(t, m.Exists(id))
+	time.Sleep(2 * time.Second)
+	assert.False(t, m.Exists(id))
 }
 
 func TestHeadStream(t *testing.T) {
@@ -35,4 +107,35 @@ func TestHeadStream(t *testing.T) {
 	// there are no new entries
 	updates, _ = next.getUpdates()
 	assert.Len(t, updates, 0)
+}
+
+type mockStore struct {
+	header  *types.Header
+	eventCh chan blockchain.Event
+}
+
+func (m *mockStore) emitEvent(evnt *blockchain.Event) {
+	m.eventCh <- *evnt
+}
+
+func (m *mockStore) Header() *types.Header {
+	return m.header
+}
+
+func (m *mockStore) GetReceiptsByHash(hash types.Hash) ([]*types.Receipt, error) {
+	return nil, nil
+}
+
+// Subscribe subscribes for chain head events
+func (m *mockStore) Subscribe() subscription {
+	return m
+}
+
+func (m *mockStore) Watch() chan blockchain.Event {
+	m.eventCh = make(chan blockchain.Event)
+	return m.eventCh
+}
+
+func (m *mockStore) Close() {
+
 }
