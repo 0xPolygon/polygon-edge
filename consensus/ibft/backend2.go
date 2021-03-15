17867aee0586efbb4bd04f88bfdb92b9aeb7c62b
Ferran Borreguero <ferranbt@protonmail.com>
2021-03-15 11:11:00 +0100

    reverting ibft directory

--- consensus/ibft/backend2.go
+++
@@ -1,9 +0,0 @@
-package ibft
-
-import "google.golang.org/grpc"
-
-// Backend2 is the IBFT consensus implementation
-type Backend2 struct {
-	grpc  grpc.Server
-	state *state
-}
--- consensus/ibft/proposals_queue_test.go
+++
@@ -1,7 +0,0 @@
-package ibft
-
-import "testing"
-
-func TestProposalsQueue(t *testing.T) {
-
-}
