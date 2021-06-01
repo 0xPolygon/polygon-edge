021bc3f786557698c8badd7ea0f81922bd0220a5
Vuksan Simunović <vuksan.simunovic@mvpworkshop.co>
2021-06-01 15:31:55 +0200

    Pass bootnodes through CLI when generating genesis.json (#47)
    * Pass bootnodes through CLI when generating genesis.json
    
    * Specify default values in the help output
    
    * Print node ID during ibft init command to make bootnode operator's life easier
    
    * Initialize bootnodes as empty slice to avoid it being null in genesis.json

--- helper/flags/flags.go
+++ helper/flags/flags.go
@@ -1,14 +1,35 @@
 package flags
 
-import "strings"
+import (
+	"github.com/multiformats/go-multiaddr"
+	"strings"
+)
+
+func formatArrayForOutput(array []string) string {
+	return "(" + strings.Join(array, ",") + ")"
+}
 
 type ArrayFlags []string
 
 func (i *ArrayFlags) String() string {
-	return "(" + strings.Join(*i, ",") + ")"
+	return formatArrayForOutput(*i)
 }
 
 func (i *ArrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+type BootnodeFlags []string
+
+func (i *BootnodeFlags) String() string {
+	return formatArrayForOutput(*i)
+}
+
+func (i *BootnodeFlags) Set(value string) error {
+	if _, err := multiaddr.NewMultiaddr(value); err != nil {
+		return err
+	}
 	*i = append(*i, value)
 	return nil
 }
