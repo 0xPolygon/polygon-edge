cbfc52ad89471716b8d540b5335cf0f873df767a
kourin <kourin1996@gmail.com>
2021-06-17 20:10:02 +0900

    Fix comment in snapshot.go

--- consensus/ibft/snapshot.go
+++ consensus/ibft/snapshot.go
@@ -49,8 +49,9 @@ func (i *Ibft) setupSnapshot() error {
 	currentEpoch, metaEpoch := (header.Number / i.epochSize), (meta.LastBlock / i.epochSize)
 	snapshot, _ := i.getSnapshot(header.Number)
 	if snapshot == nil || metaEpoch < currentEpoch {
-		// If there is no snapshot for next snapshot, restore snapshot at beginning of current epoch and update last block number
-		i.logger.Info("snapshot was not found, generate snapshot at beginning of current epoch", "current epoch", currentEpoch)
+		// Restore snapshot at the beginning of current epoch from block header
+		// if list doesn't have any snapshots to calculate snapshot for the next header
+		i.logger.Info("snapshot was not found, restore snapshot at beginning of current epoch", "current epoch", currentEpoch)
 		beginHeight := currentEpoch * i.epochSize
 		beginHeader, ok := i.blockchain.GetHeaderByNumber(beginHeight)
 		if !ok {
