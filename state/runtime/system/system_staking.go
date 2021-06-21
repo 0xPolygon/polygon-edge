1a340f12c2e62eb461c36aee4af29f64cf35aa33
Milos Zivkovic <milos.zivkovic@mvpworkshop.co>
2021-06-21 11:37:53 +0200

    Fix a double balance reduction issue

--- state/runtime/system/system_staking.go
+++ state/runtime/system/system_staking.go
@@ -1,9 +1,7 @@
 package system
 
 import (
-	"errors"
 	"fmt"
-	"runtime/debug"
 )
 
 // stakingHandler implements the staking logic for the System runtime
@@ -22,23 +20,13 @@ func (sh *stakingHandler) gas(_ []byte) uint64 {
 func (sh *stakingHandler) run(state *systemState) ([]byte, error) {
 	fmt.Printf("\n\n[Staking Handler RUN]\n\n [STAKER]: %s\n[AMOUNT]: %s\n\n", state.contract.Caller, state.contract.Value)
 
-	debug.PrintStack()
 	// Grab the value being staked
 	potentialStake := state.contract.Value
 
 	// Grab the address calling the staking method
 	staker := state.contract.Caller
 
-	// TODO Not sure if this check is necessary
-	accountBalance := state.host.GetBalance(staker)
-	if potentialStake.Cmp(accountBalance) > 0 {
-		// The staker wants to stake a balance they can't afford
-		return nil, errors.New("potential stake exceeds account balance")
-	}
-
-	// Reduce the staked amount from the account balance, 
-	// increase the staked balance
-	state.host.SubBalance(staker, potentialStake)
+	// Increase the account's staked balance
 	state.host.AddStakedBalance(staker, potentialStake)
 
 	// TODO Add the staker to the validator set after this point + checks
