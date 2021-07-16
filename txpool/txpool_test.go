073d65e35b237f7f48a4b693062f8a203d8c3c52
Lazar Travica <lazar.travica@mvpworkshop.co>
2021-07-16 17:18:06 +0200

    Added tests for the txpool heap.

--- txpool/txpool_test.go
+++ txpool/txpool_test.go
@@ -2,14 +2,13 @@ package txpool
 
 import (
 	"fmt"
-	"math/big"
-	"testing"
-
 	"github.com/0xPolygon/minimal/crypto"
 	"github.com/0xPolygon/minimal/network"
 	"github.com/0xPolygon/minimal/types"
 	"github.com/hashicorp/go-hclog"
 	"github.com/stretchr/testify/assert"
+	"math/big"
+	"testing"
 )
 
 func TestMultipleTransactions(t *testing.T) {
@@ -120,4 +119,76 @@ func TestTxnQueue_Promotion(t *testing.T) {
 	nonce, _ = pool.GetNonce(addr1)
 	assert.Equal(t, nonce, uint64(2))
 	assert.Equal(t, pool.Length(), uint64(2))
+}
+
+func TestTxnQueue_Heap(t *testing.T) {
+	type TestCase struct {
+		From     types.Address
+		GasPrice *big.Int
+		Nonce    uint64
+	}
+
+	addr1 := types.Address{0x1}
+	addr2 := types.Address{0x2}
+
+	test := func(t *testing.T, testTable []TestCase) {
+		pool, err := NewTxPool(hclog.NewNullLogger(), false, &mockStore{}, nil, nil)
+		assert.NoError(t, err)
+		pool.EnableDev()
+
+		for _, testCase := range testTable {
+			err := pool.addImpl("", &types.Transaction{
+				From:     testCase.From,
+				GasPrice: testCase.GasPrice,
+				Nonce:    testCase.Nonce,
+			})
+			assert.NoError(t, err)
+		}
+
+		for _, testCase := range testTable {
+			transaction, _ := pool.Pop()
+
+			assert.NotNil(t, transaction)
+
+			actual := TestCase{
+				From:     transaction.From,
+				GasPrice: transaction.GasPrice,
+				Nonce:    transaction.Nonce,
+			}
+
+			assert.EqualValues(t, testCase, actual)
+		}
+
+		empty, _ := pool.Pop()
+		assert.Nil(t, empty)
+	}
+
+	t.Run("the lower priced transaction should be popped first", func(t *testing.T) {
+		test(t, []TestCase{
+			{
+				From:     addr2,
+				GasPrice: big.NewInt(1),
+			},
+			{
+				From:     addr1,
+				GasPrice: big.NewInt(2),
+			},
+		})
+
+	})
+
+	t.Run("sort by nonce when same from address", func(t *testing.T) {
+		test(t, []TestCase{
+			{
+				From:     addr1,
+				GasPrice: big.NewInt(2),
+				Nonce:    0,
+			},
+			{
+				From:     addr1,
+				GasPrice: big.NewInt(1),
+				Nonce:    1,
+			},
+		})
+	})
 }
