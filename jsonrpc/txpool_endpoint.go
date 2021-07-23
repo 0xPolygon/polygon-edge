23cff99ecf1a53c1387e0284fcb2c38979bdb268
Julian Wilson <julian@nextgenbt.com>
2021-07-23 15:36:54 -0400

    Revert "Stubs"
    This reverts commit 85b58786d6f5692855e1aad72d08b093ab61b43f.

--- jsonrpc/txpool_endpoint.go
+++ jsonrpc/txpool_endpoint.go
@@ -4,15 +4,3 @@ package jsonrpc
 type Txpool struct {
 	d *Dispatcher
 }
-
-func (t *Txpool) Content() (interface{}, error) {
-	return "hello", nil
-}
-
-func (t *Txpool) Inspect() (interface{}, error) {
-	return "hello", nil
-}
-
-func (t *Txpool) Status() (interface{}, error) {
-	return "hello", nil
-}
