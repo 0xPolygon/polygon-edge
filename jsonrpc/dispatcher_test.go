c1be7bbae0ca02db19e92c6c42dafd66c1d92a90
dankostiuk <dank216@gmail.com>
2021-07-28 20:17:12 -0400

    improve test

--- jsonrpc/dispatcher_test.go
+++ jsonrpc/dispatcher_test.go
@@ -26,7 +26,7 @@ func expectJSONResult(data []byte, v interface{}) error {
 }
 
 func expectBatchJSONResult(data []byte, v interface{}) error {
-	if err := json.Unmarshal(data, v); err != nil {
+	if err := json.Unmarshal(data, &v); err != nil {
 		return err
 	}
 	return nil
@@ -168,12 +168,14 @@ func TestDispatcherBatchRequest(t *testing.T) {
 	resp, err := s.Handle([]byte(`[
     {"id":1,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x1", true]},
     {"id":2,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x2", true]},
-    {"id":3,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x3", true]}
+    {"id":3,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x3", true]},
+		{"id":4,"jsonrpc":"2.0","method": "web3_sha3","params": ["0x68656c6c6f20776f726c64"]}
 ]`))
 	assert.NoError(t, err)
 
 	var res []Response
 	assert.NoError(t, expectBatchJSONResult(resp, &res))
-	assert.Len(t, res, 3)
+	assert.Len(t, res, 4)
 	assert.Equal(t, res[0].Error, internalError)
+	assert.Nil(t, res[3].Error)
 }
