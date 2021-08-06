abbfe963e59b0f4303feec9f3c6606fcf91a7ffd
PtitLuca <61348595+PtitLuca@users.noreply.github.com>
2021-08-06 18:55:47 +0200

    Add non-nil check when submitting transaction via gRPC call (#110)
    * Add non-nil check when submitting transaction via gRPC call
    
    Signed-off-by: PtitLuca <luca.georges-francois@epitech.eu>
    
    * Add test for non-nil check in gRPC txn submission
    
    Signed-off-by: PtitLuca <luca.georges-francois@epitech.eu>
    
    * Update TxnOperator's test name
    
    Signed-off-by: PtitLuca <luca.georges-francois@epitech.eu>
    
    * Update NewTxPool call in test
    
    Signed-off-by: PtitLuca <luca.georges-francois@epitech.eu>
    
    * Fix indent in txpool tests
    
    Signed-off-by: PtitLuca <luca.georges-francois@epitech.eu>
    
    Co-authored-by: Vuksan Simunović <vuksan.simunovic@mvpworkshop.co>

--- txpool/operator.go
+++ txpool/operator.go
@@ -2,6 +2,7 @@ package txpool
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/0xPolygon/minimal/txpool/proto"
 	"github.com/0xPolygon/minimal/types"
@@ -19,6 +20,10 @@ func (t *TxPool) Status(ctx context.Context, req *empty.Empty) (*proto.TxnPoolSt
 
 // AddTxn adds a local transaction to the pool
 func (t *TxPool) AddTxn(ctx context.Context, raw *proto.AddTxnReq) (*empty.Empty, error) {
+	if raw.Raw == nil {
+		return nil, fmt.Errorf("transaction's field raw is empty")
+	}
+
 	txn := new(types.Transaction)
 	if err := txn.UnmarshalRLP(raw.Raw.Value); err != nil {
 		return nil, err
