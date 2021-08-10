d14bf635e35678d67509856863042d944bff18ef
Milos Zivkovic <milos.zivkovic@mvpworkshop.co>
2021-08-10 20:50:15 +0200

    Compartmentalize e2e txpool tests

--- e2e/txpool_test.go
+++ e2e/txpool_test.go
@@ -3,6 +3,7 @@ package e2e
 import (
 	"context"
 	"crypto/ecdsa"
+	"errors"
 	"io"
 	"math/big"
 	"testing"
@@ -88,99 +89,83 @@ func generateReq(params generateTxReqParams) *txpoolOp.AddTxnReq {
 	return msg
 }
 
-func TestTxpool_ErrNonceTooLow(t *testing.T) {
-	// Test scenario:
-	// Add tx with nonce 0
-	// -> Check if tx has been parsed
-	// Add tx with nonce 0
-	// -> tx shouldn't be added, since the nonce is too low
-
-	// Predefined values
+func TestTxPool_ErrorCodes(t *testing.T) {
 	gasPrice := big.NewInt(10000)
-
-	referenceKey, referenceAddr := tests.GenerateKeyAndAddr(t)
-	defaultBalance := framework.EthToWei(10)
-
 	devInterval := 5
 
-	// Set up the test server
-	srvs := framework.NewTestServers(t, 1, func(config *framework.TestServerConfig) {
-		config.SetConsensus(framework.ConsensusDev)
-		config.SetSeal(true)
-		config.SetDevInterval(devInterval)
-		config.Premine(referenceAddr, defaultBalance)
-	})
-	srv := srvs[0]
-
-	// TxPool client
-	clt := srv.TxnPoolOperator()
-	toAddress := types.StringToAddress("1")
-
-	// Add the initial transaction
-	addReq := generateReq(generateTxReqParams{
-		nonce:         0,
-		referenceAddr: referenceAddr,
-		referenceKey:  referenceKey,
-		toAddress:     toAddress,
-		gasPrice:      gasPrice,
-		value:         oneEth,
-		t:             t,
-	})
-
-	_, addErr := clt.AddTxn(context.Background(), addReq)
-	if addErr != nil {
-		t.Fatalf("Unable to add txn, %v", addErr)
+	testTable := []struct {
+		name           string
+		defaultBalance *big.Int
+		txValue        *big.Int
+		expectedError  error
+	}{
+		{
+			// Test scenario:
+			// Add tx with nonce 0
+			// -> Check if tx has been parsed
+			// Add tx with nonce 0
+			// -> tx shouldn't be added, since the nonce is too low
+			"ErrNonceTooLow",
+			framework.EthToWei(10),
+			oneEth,
+			txpool.ErrNonceTooLow,
+		},
+		{
+			// Test scenario:
+			// Add tx with insufficient funds
+			// -> Tx should be discarded because of low funds
+			"ErrInsufficientFunds",
+			framework.EthToWei(1),
+			framework.EthToWei(5),
+			txpool.ErrInsufficientFunds,
+		},
 	}
 
-	// Wait for the state transition to be executed
-	_ = waitForBlock(t, srv, 1, 0)
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			referenceKey, referenceAddr := tests.GenerateKeyAndAddr(t)
 
-	// Add the transaction with lower nonce value than what is
-	// currently in the world state
-	_, addErr = clt.AddTxn(context.Background(), addReq)
-	assert.NotNil(t, addErr)
-	assert.Contains(t, addErr.Error(), txpool.ErrNonceTooLow.Error())
-}
+			// Set up the test server
+			srvs := framework.NewTestServers(t, 1, func(config *framework.TestServerConfig) {
+				config.SetConsensus(framework.ConsensusDev)
+				config.SetSeal(true)
+				config.SetDevInterval(devInterval)
+				config.Premine(referenceAddr, testCase.defaultBalance)
+			})
+			srv := srvs[0]
 
-func TestTxpool_ErrInsufficientFunds(t *testing.T) {
-	// Test scenario:
-	// Add tx with insufficient funds
-	// -> Tx should be discarded because of low funds
+			// TxPool client
+			clt := srv.TxnPoolOperator()
+			toAddress := types.StringToAddress("1")
 
-	// Predefined values
-	gasPrice := big.NewInt(10000)
+			// Add the initial transaction
+			addReq := generateReq(generateTxReqParams{
+				nonce:         0,
+				referenceAddr: referenceAddr,
+				referenceKey:  referenceKey,
+				toAddress:     toAddress,
+				gasPrice:      gasPrice,
+				value:         testCase.txValue,
+				t:             t,
+			})
 
-	referenceKey, referenceAddr := tests.GenerateKeyAndAddr(t)
-	defaultBalance := framework.EthToWei(1)
+			_, addErr := clt.AddTxn(context.Background(), addReq)
 
-	devInterval := 5
+			if errors.Is(testCase.expectedError, txpool.ErrNonceTooLow) {
+				if addErr != nil {
+					t.Fatalf("Unable to add txn, %v", addErr)
+				}
 
-	// Set up the test server
-	srvs := framework.NewTestServers(t, 1, func(config *framework.TestServerConfig) {
-		config.SetConsensus(framework.ConsensusDev)
-		config.SetSeal(true)
-		config.SetDevInterval(devInterval)
-		config.Premine(referenceAddr, defaultBalance)
-	})
-	srv := srvs[0]
+				// Wait for the state transition to be executed
+				_ = waitForBlock(t, srv, 1, 0)
 
-	// TxPool client
-	clt := srv.TxnPoolOperator()
-	toAddress := types.StringToAddress("1")
+				// Add the transaction with lower nonce value than what is
+				// currently in the world state
+				_, addErr = clt.AddTxn(context.Background(), addReq)
+			}
 
-	// Add the transaction
-	addReq := generateReq(generateTxReqParams{
-		nonce:         0,
-		referenceAddr: referenceAddr,
-		referenceKey:  referenceKey,
-		toAddress:     toAddress,
-		gasPrice:      gasPrice,
-		value:         framework.EthToWei(5),
-		t:             t,
-	})
-
-	// The transaction shouldn't be added because of low funds
-	_, addErr := clt.AddTxn(context.Background(), addReq)
-	assert.NotNil(t, addErr)
-	assert.Contains(t, addErr.Error(), txpool.ErrInsufficientFunds.Error())
+			assert.NotNil(t, addErr)
+			assert.Contains(t, addErr.Error(), testCase.expectedError.Error())
+		})
+	}
 }
