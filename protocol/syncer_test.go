cdbbf9c7810c830beb2e614ebf5f401a94b6cd3d
kourin <kourin1996@gmail.com>
2021-10-08 13:07:25 +0900

    Add unit tests for BulkSyncWithPeer & WatchSyncWithPeer

--- protocol/syncer_test.go
+++ protocol/syncer_test.go
@@ -1,32 +1,35 @@
 package protocol
 
 import (
+	"errors"
 	"testing"
 	"time"
 
+	"github.com/0xPolygon/polygon-sdk/blockchain"
+	"github.com/0xPolygon/polygon-sdk/types"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHandleUser(t *testing.T) {
 	tests := []struct {
 		name       string
-		blockchain blockchainShim
+		chain      blockchainShim
 		peerChains []blockchainShim
 	}{
 		{
-			name:       "should get peer's status",
-			blockchain: NewRandomChainWithRandomHeight(t),
+			name:  "should set peer's status",
+			chain: NewRandomChain(t, 5),
 			peerChains: []blockchainShim{
-				NewRandomChainWithRandomHeight(t),
-				NewRandomChainWithRandomHeight(t),
-				NewRandomChainWithRandomHeight(t),
+				NewRandomChain(t, 5),
+				NewRandomChain(t, 10),
+				NewRandomChain(t, 15),
 			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			syncer, peerSyncers := SetupSyncerNetwork(t, tt.blockchain, tt.peerChains)
+			syncer, peerSyncers := SetupSyncerNetwork(t, tt.chain, tt.peerChains)
 
 			// Check peer's status in Syncer's peer list
 			for _, peerSyncer := range peerSyncers {
@@ -44,17 +47,17 @@ func TestHandleUser(t *testing.T) {
 func TestDeleteUser(t *testing.T) {
 	tests := []struct {
 		name                 string
-		blockchain           blockchainShim
+		chain                blockchainShim
 		peerChains           []blockchainShim
 		numDisconnectedPeers int
 	}{
 		{
-			name:       "should not have data in peers for disconnected peer",
-			blockchain: NewRandomChainWithRandomHeight(t),
+			name:  "should not have data in peers for disconnected peer",
+			chain: NewRandomChain(t, 5),
 			peerChains: []blockchainShim{
-				NewRandomChainWithRandomHeight(t),
-				NewRandomChainWithRandomHeight(t),
-				NewRandomChainWithRandomHeight(t),
+				NewRandomChain(t, 5),
+				NewRandomChain(t, 10),
+				NewRandomChain(t, 15),
 			},
 			numDisconnectedPeers: 2,
 		},
@@ -62,7 +65,7 @@ func TestDeleteUser(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			syncer, peerSyncers := SetupSyncerNetwork(t, tt.blockchain, tt.peerChains)
+			syncer, peerSyncers := SetupSyncerNetwork(t, tt.chain, tt.peerChains)
 
 			// disconnects from syncer
 			for i := 0; i < tt.numDisconnectedPeers; i++ {
@@ -72,7 +75,6 @@ func TestDeleteUser(t *testing.T) {
 
 			for idx, peerSyncer := range peerSyncers {
 				shouldBeDeleted := idx < tt.numDisconnectedPeers
-
 				peer := syncer.peers[peerSyncer.server.AddrInfo().ID]
 				if shouldBeDeleted {
 					assert.Nil(t, peer)
@@ -86,39 +88,43 @@ func TestDeleteUser(t *testing.T) {
 
 func TestBroadcast(t *testing.T) {
 	tests := []struct {
-		name          string
-		initChain     blockchainShim
-		initPeerChain blockchainShim
-		numNewBlocks  int
+		name         string
+		chain        blockchainShim
+		peerChain    blockchainShim
+		numNewBlocks int
 	}{
 		{
-			name:          "syncer should receive new block in peer",
-			initChain:     NewRandomChainWithRandomHeight(t),
-			initPeerChain: NewRandomChainWithRandomHeight(t),
-			numNewBlocks:  5,
+			name:         "syncer should receive new block in peer",
+			chain:        NewRandomChain(t, 5),
+			peerChain:    NewRandomChain(t, 10),
+			numNewBlocks: 5,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			syncer, peerSyncers := SetupSyncerNetwork(t, tt.initChain, []blockchainShim{tt.initPeerChain})
+			syncer, peerSyncers := SetupSyncerNetwork(t, tt.chain, []blockchainShim{tt.peerChain})
 			peerSyncer := peerSyncers[0]
 
 			newBlocks := GenerateNewBlocks(t, peerSyncer.blockchain, tt.numNewBlocks)
 			for _, newBlock := range newBlocks {
 				peerSyncer.Broadcast(newBlock)
-				block, ok := TryPopBlock(t, syncer, peerSyncer.server.AddrInfo().ID, 10*time.Second)
-				assert.True(t, ok, "syncer should be able to pop new block from peer %s", peerSyncer.server.AddrInfo().ID)
-				assert.Equal(t, newBlock, block, "syncer should get the same block peer broadcasted")
 			}
 
 			peerInfo := syncer.peers[peerSyncer.server.AddrInfo().ID]
 			assert.NotNil(t, peerInfo)
 
-			// latest peer's status should be based on latest peer's block
+			// Check peer's queue
+			assert.Len(t, peerInfo.enqueue, tt.numNewBlocks)
+			for _, newBlock := range newBlocks {
+				block, ok := TryPopBlock(t, syncer, peerSyncer.server.AddrInfo().ID, 10*time.Second)
+				assert.True(t, ok, "syncer should be able to pop new block from peer %s", peerSyncer.server.AddrInfo().ID)
+				assert.Equal(t, newBlock, block, "syncer should get the same block peer broadcasted")
+			}
+
+			// Check peer's status
 			lastBlock := newBlocks[len(newBlocks)-1]
-			expectedPeerStatus := BlockToStatus(lastBlock)
-			assert.Equal(t, expectedPeerStatus, peerInfo.status)
+			assert.Equal(t, BlockToStatus(lastBlock), peerInfo.status)
 		})
 	}
 }
@@ -138,6 +144,7 @@ func TestBestPeer(t *testing.T) {
 				NewRandomChain(t, 10),
 				NewRandomChain(t, 1000),
 				NewRandomChain(t, 100),
+				NewRandomChain(t, 10),
 			},
 			found:         true,
 			bestPeerIndex: 1,
@@ -146,12 +153,13 @@ func TestBestPeer(t *testing.T) {
 			name:  "should find the first peer if there are multiple candidates",
 			chain: NewRandomChain(t, 100),
 			peersChain: []blockchainShim{
+				NewRandomChain(t, 10),
 				NewRandomChain(t, 1000),
 				NewRandomChain(t, 1000),
 				NewRandomChain(t, 100),
 			},
 			found:         true,
-			bestPeerIndex: 0,
+			bestPeerIndex: 1,
 		},
 		{
 			name:  "shouldn't find if all peer doesn't have longer chain than syncer's chain",
@@ -161,8 +169,7 @@ func TestBestPeer(t *testing.T) {
 				NewRandomChain(t, 10),
 				NewRandomChain(t, 10),
 			},
-			found:         false,
-			bestPeerIndex: 0,
+			found: false,
 		},
 	}
 
@@ -176,11 +183,178 @@ func TestBestPeer(t *testing.T) {
 
 				expectedBestPeer := peerSyncers[tt.bestPeerIndex]
 				expectedBestPeerStatus := GetCurrentStatus(expectedBestPeer.blockchain)
-				assert.Equal(t, expectedBestPeer.server.AddrInfo().ID, bestPeer.peer)
+				assert.Equal(t, expectedBestPeer.server.AddrInfo().ID.String(), bestPeer.peer.String())
 				assert.Equal(t, expectedBestPeerStatus, bestPeer.status)
 			} else {
 				assert.Nil(t, bestPeer, "syncer shouldn't find best peer, but found")
 			}
+		})
+	}
+}
+
+func TestFindCommonAncestor(t *testing.T) {
+	tests := []struct {
+		name          string
+		syncerHeaders []*types.Header
+		peerHeaders   []*types.Header
+		// result
+		found       bool
+		headerIndex int
+		forkIndex   int
+		err         error
+	}{
+		{
+			name:          "should find common ancestor",
+			syncerHeaders: blockchain.NewTestHeaderChainWithSeed(nil, 10, 0),
+			peerHeaders:   blockchain.NewTestHeaderChainWithSeed(nil, 20, 0),
+			found:         true,
+			headerIndex:   9,
+			forkIndex:     10,
+			err:           nil,
+		},
+		{
+			name:          "should return error if there is no fork",
+			syncerHeaders: blockchain.NewTestHeaderChainWithSeed(nil, 11, 0),
+			peerHeaders:   blockchain.NewTestHeaderChainWithSeed(nil, 10, 0),
+			found:         false,
+			err:           errors.New("fork not found"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			chain, peerChain := blockchain.NewTestBlockchain(t, tt.syncerHeaders), blockchain.NewTestBlockchain(t, tt.peerHeaders)
+			syncer, peerSyncers := SetupSyncerNetwork(t, chain, []blockchainShim{peerChain})
+			peerSyncer := peerSyncers[0]
+
+			peer := syncer.peers[peerSyncer.server.AddrInfo().ID]
+			assert.NotNil(t, peer)
+
+			header, fork, err := syncer.findCommonAncestor(peer.client, peer.status)
+			if tt.found {
+				assert.Equal(t, tt.peerHeaders[tt.headerIndex], header)
+				assert.Equal(t, tt.peerHeaders[tt.forkIndex], fork)
+				assert.Nil(t, err)
+			} else {
+				assert.Nil(t, header)
+				assert.Nil(t, fork)
+				assert.Equal(t, tt.err, err)
+			}
+		})
+	}
+}
+
+func TestWatchSyncWithPeer(t *testing.T) {
+	tests := []struct {
+		name         string
+		headers      []*types.Header
+		peerHeaders  []*types.Header
+		numNewBlocks int
+		// result
+		synced         bool
+		expectedHeight uint64
+	}{
+		{
+			name:           "should sync until peer's latest block",
+			headers:        blockchain.NewTestHeaderChainWithSeed(nil, 10, 0),
+			peerHeaders:    blockchain.NewTestHeaderChainWithSeed(nil, 1, 0),
+			numNewBlocks:   15,
+			synced:         true,
+			expectedHeight: 15,
+		},
+		{
+			name:           "shouldn't sync",
+			headers:        blockchain.NewTestHeaderChainWithSeed(nil, 10, 0),
+			peerHeaders:    blockchain.NewTestHeaderChainWithSeed(nil, 1, 0),
+			numNewBlocks:   9,
+			synced:         false,
+			expectedHeight: 9,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			chain, peerChain := NewMockBlockchain(tt.headers), NewMockBlockchain(tt.peerHeaders)
+			syncer, peerSyncers := SetupSyncerNetwork(t, chain, []blockchainShim{peerChain})
+			peerSyncer := peerSyncers[0]
+
+			newBlocks := GenerateNewBlocks(t, peerChain, tt.numNewBlocks)
+			for _, b := range newBlocks {
+				peerSyncer.Broadcast(b)
+			}
+
+			peer := syncer.peers[peerSyncer.server.AddrInfo().ID]
+			assert.NotNil(t, peer)
+
+			latestBlock := newBlocks[len(newBlocks)-1]
+			doneCh := make(chan struct{}, 1)
+			go func() {
+				syncer.WatchSyncWithPeer(peer, func(b *types.Block) bool {
+					// sync until latest block
+					return b.Header.Number < latestBlock.Header.Number
+				})
+				// wait until syncer updates status by latest block
+				WaitUntilProcessedAllEvents(t, syncer, 10*time.Second)
+				doneCh <- struct{}{}
+			}()
+
+			select {
+			case <-doneCh:
+				assert.True(t, tt.synced, "syncer shouldn't sync any block with peer, but did")
+				assert.Equal(t, BlockToStatus(latestBlock), syncer.status)
+				assert.Equal(t, tt.expectedHeight, syncer.status.Number)
+				break
+			case <-time.After(time.Second * 10):
+				assert.False(t, tt.synced, "syncer should sync blocks with peer, but didn't")
+				break
+			}
+		})
+	}
+}
+
+func TestBulkSyncWithPeer(t *testing.T) {
+	tests := []struct {
+		name        string
+		headers     []*types.Header
+		peerHeaders []*types.Header
+		// result
+		synced bool
+		err    error
+	}{
+		{
+			name:        "should sync until peer's latest block",
+			headers:     blockchain.NewTestHeaderChainWithSeed(nil, 10, 0),
+			peerHeaders: blockchain.NewTestHeaderChainWithSeed(nil, 20, 0),
+			synced:      true,
+			err:         nil,
+		},
+		{
+			name:        "should sync until peer's latest block",
+			headers:     blockchain.NewTestHeaderChainWithSeed(nil, 20, 0),
+			peerHeaders: blockchain.NewTestHeaderChainWithSeed(nil, 10, 0),
+			synced:      false,
+			err:         errors.New("fork not found"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			chain, peerChain := NewMockBlockchain(tt.headers), NewMockBlockchain(tt.peerHeaders)
+			syncer, peerSyncers := SetupSyncerNetwork(t, chain, []blockchainShim{peerChain})
+			peerSyncer := peerSyncers[0]
+
+			peer := syncer.peers[peerSyncer.server.AddrInfo().ID]
+			assert.NotNil(t, peer)
+
+			err := syncer.BulkSyncWithPeer(peer)
+			assert.Equal(t, tt.err, err)
+			WaitUntilProcessedAllEvents(t, syncer, 10*time.Second)
+
+			expectedStatus := HeaderToStatus(tt.headers[len(tt.headers)-1])
+			if tt.synced {
+				expectedStatus = HeaderToStatus(tt.peerHeaders[len(tt.peerHeaders)-1])
+			}
+			assert.Equal(t, expectedStatus, syncer.status)
 		})
 	}
 }
