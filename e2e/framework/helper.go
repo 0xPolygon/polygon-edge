478b09dbc787c273559c206eecf9b30aee9540be
Milos Zivkovic <milos.zivkovic@mvpworkshop.co>
2021-10-12 16:13:32 +0200

    Refactor EthToWei to reuse code

--- e2e/framework/helper.go
+++ e2e/framework/helper.go
@@ -24,9 +24,7 @@ import (
 )
 
 func EthToWei(ethValue int64) *big.Int {
-	return new(big.Int).Mul(
-		big.NewInt(ethValue),
-		new(big.Int).Exp(big.NewInt(10), big.NewInt(18), nil))
+	return EthToWeiPrecise(ethValue, 18)
 }
 
 func EthToWeiPrecise(ethValue int64, decimals int64) *big.Int {
