74f2dc6f637387c21f547695b4030058bff0fe07
munna <munnasitu0908@gmail.com>
2021-11-02 22:02:16 +0530

    added dns address to addressfactory of host

--- network/server.go
+++ network/server.go
@@ -28,9 +28,11 @@ type Config struct {
 	NoDiscover bool
 	Addr       *net.TCPAddr
 	NatAddr    net.IP
+	Dns        multiaddr.Multiaddr
 	DataDir    string
 	MaxPeers   uint64
-	Chain      *chain.Chain
+
+	Chain *chain.Chain
 }
 
 func DefaultConfig() *Config {
@@ -96,6 +98,8 @@ func NewServer(logger hclog.Logger, config *Config) (*Server, error) {
 			if addr != nil {
 				addrs = []multiaddr.Multiaddr{addr}
 			}
+		} else if config.Dns != nil {
+			addrs = []multiaddr.Multiaddr{config.Dns}
 		}
 
 		return addrs
