fcfdb4c8c324294ef06cdb27871464c4253b1760
Luca Georges Francois <luca.georges-francois@epitech.eu>
2021-11-26 14:57:21 +0100

    Add value flag for loadbot
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- command/loadbot/loadbot_command.go
+++ command/loadbot/loadbot_command.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/0xPolygon/polygon-sdk/command/helper"
+	"github.com/0xPolygon/polygon-sdk/types"
 	"github.com/mitchellh/cli"
 )
 
@@ -29,6 +30,15 @@ func (l *LoadbotCommand) DefineFlags() {
 		Description: "How many accounts must be used by the loadbot to send transactions. Default: 1000",
 		Arguments: []string{
 			"ACCOUNTS_COUNT",
+		},
+		ArgumentsOptional: true,
+	}
+
+	l.FlagMap["value"] = helper.FlagDescriptor{
+		Description: "The value sent in each transaction in wei. If negative, " +
+			"a random value will be generated. Default: 100",
+		Arguments: []string{
+			"VALUE",
 		},
 		ArgumentsOptional: true,
 	}
@@ -58,19 +68,33 @@ func (l *LoadbotCommand) Run(args []string) int {
 	// Placeholders for flags
 	var tps uint64
 	var accountsCount uint64
+	var valueRaw string
 
 	// Map flags to placeholders
 	flags.Uint64Var(&tps, "tps", 1000, "")
 	flags.Uint64Var(&accountsCount, "accountsCount", 1000, "")
+	flags.StringVar(&valueRaw, "value", "-1", "")
 
+	var err error
 	// Parse cli arguments
-	if err := flags.Parse(args); err != nil {
+	if err = flags.Parse(args); err != nil {
 		l.UI.Error(fmt.Sprintf("failed to parse args: %v", err))
 		return 1
 	}
 
+	// Trying to parse value is a custom one is provided
+	var value int64 = -1
+	if valueRaw != "-1" {
+		value, err = types.ParseInt64orHex(&valueRaw)
+		if err != nil {
+			l.UI.Error(fmt.Sprintf("failed to parse value: %v", err))
+			return 1
+		}
+	}
+
 	fmt.Println("TPS:", tps)
-	fmt.Println("Accounts count", accountsCount)
+	fmt.Println("Accounts count:", accountsCount)
+	fmt.Println("Value:", value)
 
 	return 0
 }
