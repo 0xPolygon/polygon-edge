b44f050a26f3e507a9cf511328a2dadaa3443b61
Luca Georges Francois <luca.georges-francois@epitech.eu>
2021-11-26 17:33:31 +0100

    Add clients generator for loadbot
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- command/loadbot/execution.go
+++ command/loadbot/execution.go
@@ -3,6 +3,9 @@ package loadbot
 import (
 	"crypto/rand"
 	"fmt"
+	txpoolOp "github.com/0xPolygon/polygon-sdk/txpool/proto"
+	"github.com/umbracle/go-web3/jsonrpc"
+	"google.golang.org/grpc"
 	"math/big"
 	"time"
 )
@@ -32,6 +35,32 @@ func generateRandomValue() (*big.Int, error) {
 	return b, nil
 }
 
+func createJsonRpcClient(endpoint string) (*jsonrpc.Client, error) {
+	client, err := jsonrpc.NewClient(endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new JSON RPC client: %v", err)
+	}
+	return client, nil
+}
+
+func createGRpcClient(endpoint string) (*txpoolOp.TxnPoolOperatorClient, error) {
+	conn, err := grpc.Dial(endpoint, grpc.WithInsecure())
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gRPC connection: %v", err)
+	}
+
+	client := txpoolOp.NewTxnPoolOperatorClient(conn)
+	return &client, nil
+}
+
 func Run(conf *Configuration) (error, *Metrics) {
-	return nil, nil
+	ticker := time.NewTicker(1 * time.Second / time.Duration(conf.TPS))
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			return nil, nil
+		}
+	}
 }
