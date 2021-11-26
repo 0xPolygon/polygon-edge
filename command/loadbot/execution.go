ef047178574b795b36cf6cbd1c8bb1f4cab3f362
Luca Georges Francois <luca.georges-francois@epitech.eu>
2021-11-26 17:24:08 +0100

    Add random value generator for the loadbot
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- command/loadbot/execution.go
+++ command/loadbot/execution.go
@@ -1,6 +1,11 @@
 package loadbot
 
-import "time"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"time"
+)
 
 type Configuration struct {
 	TPS           uint64
@@ -15,6 +20,16 @@ type Metrics struct {
 	Duration                   time.Duration
 	TotalTransactionsSentCount uint64
 	FailedTransactionsCount    uint64
+}
+
+// generateRandomValue creates a random value used in a transaction.
+// The max value that can be generated represents 0.01 ETH.
+func generateRandomValue() (*big.Int, error) {
+	b, err := rand.Int(rand.Reader, big.NewInt(10000000000000000))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create random number: %v", err)
+	}
+	return b, nil
 }
 
 func Run(conf *Configuration) (error, *Metrics) {
