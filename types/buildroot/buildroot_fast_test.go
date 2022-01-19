4c90fc91d3e3675ae2dcf6ed7b74d18adb05f621
munna <munnasitu0908@gmail.com>
2022-01-20 03:20:23 +0530

    build fix

--- types/buildroot/buildroot_fast_test.go
+++ types/buildroot/buildroot_fast_test.go
@@ -5,7 +5,8 @@ import (
 	"crypto/rand"
 	"math/big"
 	"testing"
-	"github.com/0xPolygon/polygon-sdk/helper/keccak"
+
+	"github.com/0xPolygon/polygon-edge/helper/keccak"
 )
 
 func BenchmarkFast(b *testing.B) {
