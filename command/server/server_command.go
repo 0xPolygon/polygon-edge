da4fb9e33c78d88127c36d2f8088595d29f7f476
munna <munnasitu0908@gmail.com>
2022-01-20 02:27:31 +0530

    updated flag info

--- command/server/server_command.go
+++ command/server/server_command.go
@@ -144,14 +144,16 @@ func (c *ServerCommand) DefineFlags() {
 	}
 
 	c.FlagMap["max-inbound-peers"] = helper.FlagDescriptor{
-		Description: "Sets the client's maximum number of inbound peers allowded. Default: 40",
+		Description: fmt.Sprintf("Sets the client's max no.of inbound peers allowded. Default: %d",
+			helper.DefaultConfig().Network.MaxInboundPeers),
 		Arguments: []string{
 			"PEER_COUNT",
 		},
 		FlagOptional: true,
 	}
 	c.FlagMap["max-outbound-peers"] = helper.FlagDescriptor{
-		Description: "Sets the client's max no.of outbound peers allowded. Default: 10",
+		Description: fmt.Sprintf("Sets the client's max no.of outbound peers allowded. Default: %d",
+			helper.DefaultConfig().Network.MaxOutboundPeers),
 		Arguments: []string{
 			"PEER_COUNT",
 		},
