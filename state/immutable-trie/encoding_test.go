cc8e62e226573b5bebc03030dc86068e776705dd
Milos Zivkovic <milos.zivkovic@mvpworkshop.co>
2022-01-25 12:58:31 +0100

    Add coverage tests for KeyBytesToHexNibbles

--- state/immutable-trie/encoding_test.go
+++ state/immutable-trie/encoding_test.go
@@ -123,3 +123,65 @@ func TestEncoding_NibblesToBytes(t *testing.T) {
 		})
 	}
 }
+
+func TestEncoding_KeyBytesToHexNibbles(t *testing.T) {
+	termSignal := byte(0x10) // 16
+
+	testTable := []struct {
+		name           string
+		inputString    []byte
+		expectedOutput []byte
+	}{
+		{
+			"Valid case #1",
+			[]byte{0xa},
+			[]byte{
+				0x0, 0xa,
+				termSignal,
+			},
+		},
+		{
+			"Valid case #2",
+			[]byte{0xa, 0xb, 0xc},
+			[]byte{
+				0x0, 0xa,
+				0x0, 0xb,
+				0x0, 0xc,
+				termSignal,
+			},
+		},
+		{
+			"Valid case #3",
+			[]byte("Polygon"),
+			[]byte{
+				0x5, 0x0, // P -> 85 	-> 0x50
+				0x6, 0xf, // o -> 111 	-> 0x6f
+				0x6, 0xc, // l -> 108 	-> 0x6c
+				0x7, 0x9, // y -> 121 	-> 0x79
+				0x6, 0x7, // g -> 103 	-> 0x67
+				0x6, 0xf, // o -> 111 	-> 0x6f
+				0x6, 0xe, // n -> 110 	-> 0x6e
+				termSignal,
+			},
+		},
+		{
+			"Valid case #4",
+			[]byte{},
+			[]byte{termSignal},
+		},
+		{
+			"Valid case #5",
+			nil,
+			[]byte{termSignal},
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			output := keybytesToHex(testCase.inputString)
+
+			assert.Len(t, output, len(testCase.expectedOutput))
+			assert.Equal(t, testCase.expectedOutput, output)
+		})
+	}
+}
