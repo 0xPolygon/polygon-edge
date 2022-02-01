545a4d56209e885abb3b1173ac52becd5e5d3046
Akshar Dave <akshar314@gmail.com>
2022-02-01 10:10:30 -0500

    fix lint error

--- network/server.go
+++ network/server.go
@@ -767,6 +767,7 @@ var (
 	// /ip6/<any loopback>/tcp/<port>
 	// /dns/foobar.com/tcp/<port>
 	loopbackRegex = regexp.MustCompile(
+		//nolint:lll
 		`^\/ip4\/127(?:\.[0-9]+){0,2}\.[0-9]+\/tcp\/\d+$|^\/ip4\/localhost\/tcp\/\d+$|^\/ip6\/(?:0*\:)*?:?0*1\/tcp\/\d+$|^\/dns\/[^-|^/][A-Za-z0-9-]([^-|^/]?)+([\\-\\.]{1}[a-z0-9]+)*\\.[A-Za-z]{2,}(/?)$`,
 	)
 )
