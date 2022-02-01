056b294682b6d568f331c3f526378d9e0cb0650b
Akshar Dave <akshar314@gmail.com>
2022-01-31 19:20:14 -0500

    fix type

--- network/server.go
+++ network/server.go
@@ -765,7 +765,7 @@ var (
 	// /ip4/127.0.0.1/tcp/<port>
 	// /ip4/<any other loopback>/tcp/<port>
 	// /ip6/<any loopback>/tcp/<port>
-	// /dns/foobar.com/tpc/<port>
+	// /dns/foobar.com/tcp/<port>
 	loopbackRegex = regexp.MustCompile(
 		`^\/ip4\/127(?:\.[0-9]+){0,2}\.[0-9]+\/tcp\/\d+$|^\/ip4\/localhost\/tcp\/\d+$|^\/ip6\/(?:0*\:)*?:?0*1\/tcp\/\d+$|^\/dns\/[^-|^/][A-Za-z0-9-]([^-|^/]?)+([\\-\\.]{1}[a-z0-9]+)*\\.[A-Za-z]{2,}(/?)$`,
 	)
