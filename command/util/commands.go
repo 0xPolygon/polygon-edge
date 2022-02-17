bd192e4677a2b15e67765c3949c13d1e43d945eb
kourin <kourin1996@gmail.com>
2022-02-17 17:36:37 +0900

    IBFT PoA/PoS switch (#334)
    * Add PoA/PoS switch in IBFT
    
    * Refactor IBFT fork addition
    
    * Fix lint error
    
    * Add output in ibft switch command
    
    * Remove IsAvailable call in each hook in IBFT
    
    * Refactor parsing IBFT forks from genesis.json
    
    * Rename method name to proper one
    
    * Correct wrong output in ibft switch
    
    * Fix lint error
    
    * Add tests around PoA/PoS switch
    
    * Fix wrong type in test
    
    * Add e2e test for PoA/PoS switch
    
    * Fix wrong result format in ibft switch
    
    * Fix lint error
    
    * Fix build error
    
    * Improve code readability
    
    * Fix build error
    
    * Remove leftover comment
    
    * Fix typo
    
    * Move base IBFT mechanism definition to hooks.go
    
    * Fix lint error

--- command/util/commands.go
+++ command/util/commands.go
@@ -46,6 +46,7 @@ func Commands() map[string]cli.CommandFactory {
 	ibftProposeCmd := ibft.IbftPropose{Base: base, Formatter: formatter, GRPC: grpc}
 	ibftSnapshotCmd := ibft.IbftSnapshot{Base: base, Formatter: formatter, GRPC: grpc}
 	ibftStatusCmd := ibft.IbftStatus{Base: base, Formatter: formatter, GRPC: grpc}
+	ibftSwitchCmd := ibft.IBFTSwitch{Base: base, Formatter: formatter}
 
 	peersCmd := peers.PeersCommand{}
 	peersAddCmd := peers.PeersAdd{Base: base, Formatter: formatter, GRPC: grpc}
@@ -108,6 +109,9 @@ func Commands() map[string]cli.CommandFactory {
 		},
 		ibftStatusCmd.GetBaseCommand(): func() (cli.Command, error) {
 			return &ibftStatusCmd, nil
+		},
+		ibftSwitchCmd.GetBaseCommand(): func() (cli.Command, error) {
+			return &ibftSwitchCmd, nil
 		},
 
 		// TXPOOL COMMANDS //
