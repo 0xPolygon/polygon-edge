bac6047ea80243c7eabe7ad4b8224fe1a4ad058e
dbrajovic <dbrajovic3@gmail.com>
2022-03-28 15:53:25 +0200

    remove TODO

--- bridge/checkpoint/transport/transport.go
+++ bridge/checkpoint/transport/transport.go
@@ -61,11 +61,6 @@ func (t *libp2pGossipTransport) SendNoAck(noAck *NoAckMessage) error {
 
 func (t *libp2pGossipTransport) Subscribe(handler func(interface{})) error {
 	return t.topic.Subscribe(func(obj interface{}) {
-		// TODO: convert proto.Message to CheckpointMessage,
-		// AckMessage, or NoAckMessage defined in bridge/checkpoint/transport/types.go
-
-		// and call handler
-
 		protoMsg, ok := obj.(*proto.CheckpointMessage)
 		if !ok {
 			t.logger.Error("received unexpected typed message", "message", obj)
