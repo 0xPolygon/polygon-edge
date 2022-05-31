dcecbafaff88bc8e0b45b5007b155b9df0ccfebc
dbrajovic <dbrajovic3@gmail.com>
2022-05-31 11:56:41 +0200

    t.Parallel

--- txpool/txpool_test.go
+++ txpool/txpool_test.go
@@ -1153,15 +1153,17 @@ func TestDemote(t *testing.T) {
 }
 
 func TestEnqueuedPruning(t *testing.T) {
+	t.Parallel()
+
 	t.Run(
 		"prune stale tx",
 		func(t *testing.T) {
-			//	create pool
+			t.Parallel()
+
 			pool, err := newTestPool()
 			assert.NoError(t, err)
 			pool.SetSigner(&mockSigner{})
 
-			//	enqueue some txs
 			go func() {
 				err := pool.addTx(local, newTx(addr1, 5, 1))
 				assert.NoError(t, err)
@@ -1169,29 +1171,28 @@ func TestEnqueuedPruning(t *testing.T) {
 			pool.handleEnqueueRequest(<-pool.enqueueReqCh)
 
 			acc := pool.accounts.get(addr1)
+			acc.lastPromoted = time.Now().Add(-3 * time.Hour) // fake lastPromoted
 
 			assert.Equal(t, uint64(1), acc.enqueued.length())
-
-			//	fake lastPromoted
-			acc.lastPromoted = time.Now().Add(-3 * time.Hour)
 
 			//	pretend 3 hours have passed
 			//	and trigger the pruning cycle
 			pool.pruneStaleAccounts()
 
-			//	enqueued txs are removed
+			//	enqueued tx is not removed
 			assert.Equal(t, uint64(0), acc.enqueued.length())
 		})
 
 	t.Run(
 		"no stale tx to prune",
 		func(t *testing.T) {
+			t.Parallel()
+
 			//	create pool
 			pool, err := newTestPool()
 			assert.NoError(t, err)
 			pool.SetSigner(&mockSigner{})
 
-			//	enqueue some txs
 			go func() {
 				err := pool.addTx(local, newTx(addr1, 5, 1))
 				assert.NoError(t, err)
@@ -1199,20 +1200,17 @@ func TestEnqueuedPruning(t *testing.T) {
 			pool.handleEnqueueRequest(<-pool.enqueueReqCh)
 
 			acc := pool.accounts.get(addr1)
+			acc.lastPromoted = time.Now().Add(-5 * time.Second) // fake lastPromoted
 
 			assert.Equal(t, uint64(1), acc.enqueued.length())
-
-			//	fake lastPromoted
-			acc.lastPromoted = time.Now().Add(-5 * time.Second)
 
 			//	pretend 3 hours have passed
 			//	and trigger the pruning cycle
 			pool.pruneStaleAccounts()
 
-			//	enqueued txs are removed
+			//	enqueued tx is removed
 			assert.Equal(t, uint64(1), acc.enqueued.length())
 		})
-
 }
 
 /* "Integrated" tests */
