06dc1551f58df081d41e0fd3e68233d8312b5cb0
Miloš Živković <milos.zivkovic@mvpworkshop.co>
2022-06-01 10:37:15 +0200

    [Blockchain] Simplify block verification (#544)
    * Add the initial block verification separation
    
    * Separate out consensus and other module verification methods
    
    * Add better naming to block verification methods
    
    * Standardize block result errors
    
    * Add receipts extraction
    
    * Add additional documentation for the usage of the receipts cache
    
    * Add delegators for testing the blockchain module
    
    * Add mock support for the blockchain and storage modules
    
    * Add coverage tests
    
    * Simplify the gas verification
    
    * Rearrange code blocks
    
    * Rename some helper methods to be more clear
    
    * Make the mock testing structure public, refactor a simple test
    
    * Remove redundant comment
    
    * Simplify the error variable in initCaches
    
    * Eliminate magic numbers
    
    * Rearrange the checks in VerifySealedBlock
    
    * Remove reference block type when verifying
    
    * Add explainer for WriteBlock
    
    * Verify the header seals before inserting the block
    
    * Rename interface methods to be more implementation agnostic

--- blockchain/testing.go
+++ blockchain/testing.go
@@ -1,7 +1,9 @@
 package blockchain
 
 import (
+	"errors"
 	"fmt"
+	"github.com/0xPolygon/polygon-edge/blockchain/storage"
 	"math/big"
 	"testing"
 
@@ -11,7 +13,6 @@ import (
 	"github.com/hashicorp/go-hclog"
 
 	"github.com/0xPolygon/polygon-edge/types"
-	"github.com/0xPolygon/polygon-edge/types/buildroot"
 )
 
 var (
@@ -19,8 +20,12 @@ var (
 	defaultBlockGasTarget uint64 = 8000000
 )
 
-// NewTestHeaderChainWithSeed creates a new chain with a seed factor
-func NewTestHeaderChainWithSeed(genesis *types.Header, n int, seed uint64) []*types.Header {
+var (
+	errInvalidTypeAssertion = errors.New("invalid type assertion")
+)
+
+// NewTestHeadersWithSeed creates a new chain with a seed factor
+func NewTestHeadersWithSeed(genesis *types.Header, n int, seed uint64) []*types.Header {
 	head := func(i int64) *types.Header {
 		return &types.Header{
 			Number:       uint64(i),
@@ -51,20 +56,20 @@ func NewTestHeaderChainWithSeed(genesis *types.Header, n int, seed uint64) []*ty
 	return headers
 }
 
-// NewTestHeaderChain creates a chain of valid headers
-func NewTestHeaderChain(n int) []*types.Header {
-	return NewTestHeaderChainWithSeed(nil, n, 0)
+// NewTestHeaders creates a chain of valid headers
+func NewTestHeaders(n int) []*types.Header {
+	return NewTestHeadersWithSeed(nil, n, 0)
 }
 
-// NewTestHeaderFromChain creates n new headers from an already existing chain
-func NewTestHeaderFromChain(headers []*types.Header, n int) []*types.Header {
-	return NewTestHeaderFromChainWithSeed(headers, n, 0)
+// AppendNewTestHeaders creates n new headers from an already existing chain
+func AppendNewTestHeaders(headers []*types.Header, n int) []*types.Header {
+	return AppendNewTestheadersWithSeed(headers, n, 0)
 }
 
-// NewTestHeaderFromChainWithSeed creates n new headers from an already existing chain
-func NewTestHeaderFromChainWithSeed(headers []*types.Header, n int, seed uint64) []*types.Header {
+// AppendNewTestheadersWithSeed creates n new headers from an already existing chain
+func AppendNewTestheadersWithSeed(headers []*types.Header, n int, seed uint64) []*types.Header {
 	// We do +1 because the first header will be the genesis we supplied
-	newHeaders := NewTestHeaderChainWithSeed(headers[len(headers)-1], n+1, seed)
+	newHeaders := NewTestHeadersWithSeed(headers[len(headers)-1], n+1, seed)
 
 	preHeaders := make([]*types.Header, len(headers))
 	copy(preHeaders, headers)
@@ -79,74 +84,6 @@ func HeadersToBlocks(headers []*types.Header) []*types.Block {
 	}
 
 	return blocks
-}
-
-// NewTestBodyChain creates a test blockchain with headers, body and receipts
-func NewTestBodyChain(n int) ([]*types.Header, []*types.Block, [][]*types.Receipt) {
-	genesis := &types.Block{
-		Header: &types.Header{
-			Number:   0,
-			GasLimit: uint64(0),
-		},
-	}
-	genesis.Header.ComputeHash()
-
-	blocks := []*types.Block{genesis}
-	receipts := [][]*types.Receipt{nil} // genesis does not have tx
-
-	for i := 1; i < n; i++ {
-		header := &types.Header{
-			ParentHash: blocks[i-1].Hash(),
-			Number:     uint64(i),
-			Difficulty: uint64(i),
-			ExtraData:  []byte{},
-		}
-		header.ComputeHash()
-
-		// -- txs ---
-
-		addr0 := types.StringToAddress("00")
-		t0 := &types.Transaction{
-			Nonce:    uint64(i),
-			To:       &addr0,
-			Value:    big.NewInt(0),
-			Gas:      0,
-			GasPrice: big.NewInt(0),
-			Input:    header.Hash.Bytes(),
-			V:        big.NewInt(27),
-		}
-		t0.ComputeHash()
-
-		txs := []*types.Transaction{t0}
-
-		// -- receipts --
-		r0 := &types.Receipt{
-			GasUsed: uint64(i),
-			//TxHash:            t0.Hash,
-			CumulativeGasUsed: uint64(i), // this value changes the rlpHash
-		}
-		localReceipts := []*types.Receipt{r0}
-
-		header.TxRoot = buildroot.CalculateTransactionsRoot(txs)
-		header.ReceiptsRoot = buildroot.CalculateReceiptsRoot(localReceipts)
-		header.LogsBloom = types.CreateBloom(localReceipts)
-		header.Sha3Uncles = types.EmptyUncleHash
-
-		block := &types.Block{
-			Header:       header,
-			Transactions: txs,
-		}
-
-		blocks = append(blocks, block)
-		receipts = append(receipts, localReceipts)
-	}
-
-	headers := []*types.Header{}
-	for _, block := range blocks {
-		headers = append(headers, block.Header)
-	}
-
-	return headers, blocks, receipts
 }
 
 // NewTestBlockchain creates a new dummy blockchain for testing
@@ -190,26 +127,165 @@ func NewTestBlockchain(t *testing.T, headers []*types.Header) *Blockchain {
 	return b
 }
 
-type MockVerifier struct {
+type TestCallbackType string
+
+const (
+	VerifierCallback TestCallbackType = "VerifierCallback"
+	ExecutorCallback TestCallbackType = "ExecutorCallback"
+	ChainCallback    TestCallbackType = "ChainCallback"
+	StorageCallback  TestCallbackType = "StorageCallback"
+)
+
+// NewMockBlockchain constructs a new mock blockchain
+func NewMockBlockchain(
+	callbackMap map[TestCallbackType]interface{},
+) (*Blockchain, error) {
+	var (
+		mockVerifier = &MockVerifier{}
+		executor     = &mockExecutor{}
+		config       = &chain.Chain{
+			Genesis: &chain.Genesis{
+				Number:   0,
+				GasLimit: 0,
+			},
+			Params: &chain.Params{
+				Forks: chain.AllForksEnabled,
+			},
+		}
+		mockStorage = storage.NewMockStorage()
+	)
+
+	// Set up the mocks and callbacks
+	if callbackMap != nil {
+		// Execute the verifier callback
+		if verifierCallback, ok := callbackMap[VerifierCallback]; ok {
+			callback, ok := verifierCallback.(func(verifier *MockVerifier))
+			if !ok {
+				return nil, errInvalidTypeAssertion
+			}
+
+			callback(mockVerifier)
+		}
+
+		// Execute the executor callback
+		if executorCallback, ok := callbackMap[ExecutorCallback]; ok {
+			callback, ok := executorCallback.(func(executor *mockExecutor))
+			if !ok {
+				return nil, errInvalidTypeAssertion
+			}
+
+			callback(executor)
+		}
+
+		// Execute the chain config callback
+		if chainCallback, ok := callbackMap[ChainCallback]; ok {
+			callback, ok := chainCallback.(func(chain *chain.Chain))
+			if !ok {
+				return nil, errInvalidTypeAssertion
+			}
+
+			callback(config)
+		}
+
+		// Execute the storage callback
+		if storageCallback, ok := callbackMap[StorageCallback]; ok {
+			callback, ok := storageCallback.(func(storage *storage.MockStorage))
+			if !ok {
+				return nil, errInvalidTypeAssertion
+			}
+
+			callback(mockStorage)
+		}
+	}
+
+	blockchain := &Blockchain{
+		logger:    hclog.NewNullLogger(),
+		db:        mockStorage,
+		consensus: mockVerifier,
+		executor:  executor,
+		config:    config,
+		stream:    &eventStream{},
+		gpAverage: &gasPriceAverage{
+			price: big.NewInt(0),
+			count: big.NewInt(0),
+		},
+	}
+
+	if err := blockchain.initCaches(10); err != nil {
+		return nil, err
+	}
+
+	return blockchain, nil
 }
 
-func (m *MockVerifier) VerifyHeader(parent, header *types.Header) error {
+// Verifier delegators
+
+type verifyHeaderDelegate func(*types.Header) error
+type processHeadersDelegate func([]*types.Header) error
+type getBlockCreatorDelegate func(*types.Header) (types.Address, error)
+type preStateCommitDelegate func(*types.Header, *state.Transition) error
+
+type MockVerifier struct {
+	verifyHeaderFn    verifyHeaderDelegate
+	processHeadersFn  processHeadersDelegate
+	getBlockCreatorFn getBlockCreatorDelegate
+	preStateCommitFn  preStateCommitDelegate
+}
+
+func (m *MockVerifier) VerifyHeader(header *types.Header) error {
+	if m.verifyHeaderFn != nil {
+		return m.verifyHeaderFn(header)
+	}
+
 	return nil
+}
+
+func (m *MockVerifier) HookVerifyHeader(fn verifyHeaderDelegate) {
+	m.verifyHeaderFn = fn
 }
 
 func (m *MockVerifier) ProcessHeaders(headers []*types.Header) error {
+	if m.processHeadersFn != nil {
+		return m.processHeadersFn(headers)
+	}
+
 	return nil
+}
+
+func (m *MockVerifier) HookProcessHeaders(fn processHeadersDelegate) {
+	m.processHeadersFn = fn
 }
 
 func (m *MockVerifier) GetBlockCreator(header *types.Header) (types.Address, error) {
+	if m.getBlockCreatorFn != nil {
+		return m.getBlockCreatorFn(header)
+	}
+
 	return header.Miner, nil
 }
 
+func (m *MockVerifier) HookGetBlockCreator(fn getBlockCreatorDelegate) {
+	m.getBlockCreatorFn = fn
+}
+
 func (m *MockVerifier) PreStateCommit(header *types.Header, txn *state.Transition) error {
+	if m.preStateCommitFn != nil {
+		return m.preStateCommitFn(header, txn)
+	}
+
 	return nil
 }
 
+func (m *MockVerifier) HookPreStateCommit(fn preStateCommitDelegate) {
+	m.preStateCommitFn = fn
+}
+
+// Executor delegators
+
+type processBlockDelegate func(types.Hash, *types.Block, types.Address) (*state.Transition, error)
+
 type mockExecutor struct {
+	processBlockFn processBlockDelegate
 }
 
 func (m *mockExecutor) ProcessBlock(
@@ -217,7 +293,15 @@ func (m *mockExecutor) ProcessBlock(
 	block *types.Block,
 	blockCreator types.Address,
 ) (*state.Transition, error) {
+	if m.processBlockFn != nil {
+		return m.processBlockFn(parentRoot, block, blockCreator)
+	}
+
 	return nil, nil
+}
+
+func (m *mockExecutor) HookProcessBlock(fn processBlockDelegate) {
+	m.processBlockFn = fn
 }
 
 func TestBlockchain(t *testing.T, genesis *chain.Genesis) *Blockchain {
