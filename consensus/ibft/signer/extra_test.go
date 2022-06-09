4ba1feaf1bdaa053ece5b8a4c3ffd7ecd4a19362
kourin <kourin.code@gmail.com>
2022-06-09 12:40:42 +0300

    Uncomment extra test

--- consensus/ibft/signer/extra_test.go
+++ consensus/ibft/signer/extra_test.go
@@ -1,199 +1,226 @@
 package signer
 
+import (
+	"crypto/ecdsa"
+	"reflect"
+	"testing"
+
+	"github.com/0xPolygon/polygon-edge/consensus/ibft/validators"
+	"github.com/0xPolygon/polygon-edge/crypto"
+	"github.com/0xPolygon/polygon-edge/helper/tests"
+	"github.com/0xPolygon/polygon-edge/types"
+	"github.com/stretchr/testify/assert"
+)
+
 // useIstanbulHeaderHash is a helper function so that test use istanbulHeaderHash during the test
-// func useIstanbulHeaderHash(t *testing.T, signer Signer) {
-// 	t.Helper()
+func useIstanbulHeaderHash(t *testing.T, signer Signer) {
+	t.Helper()
 
-// 	originalHashCalc := types.HeaderHash
-// 	types.HeaderHash = func(h *types.Header) types.Hash {
-// 		hash, err := signer.CalculateHeaderHash(h)
-// 		if err != nil {
-// 			return types.ZeroHash
-// 		}
+	originalHashCalc := types.HeaderHash
+	types.HeaderHash = func(h *types.Header) types.Hash {
+		hash, err := signer.CalculateHeaderHash(h)
+		if err != nil {
+			return types.ZeroHash
+		}
 
-// 		return hash
-// 	}
+		return hash
+	}
 
-// 	t.Cleanup(func() {
-// 		types.HeaderHash = originalHashCalc
-// 	})
-// }
+	t.Cleanup(func() {
+		types.HeaderHash = originalHashCalc
+	})
+}
 
-// func TestExtraEncoding(t *testing.T) {
-// 	seal1 := types.StringToHash("1").Bytes()
-// 	seal2 := types.StringToHash("2").Bytes()
+func TestExtraEncoding(t *testing.T) {
+	seal1 := types.StringToHash("1").Bytes()
+	seal2 := types.StringToHash("2").Bytes()
 
-// 	cases := []struct {
-// 		data *IstanbulExtra
-// 	}{
-// 		{
-// 			data: &IstanbulExtra{
-// 				Validators: []types.Address{
-// 					types.StringToAddress("1"),
-// 				},
-// 				Seal: seal1,
-// 				CommittedSeal: &SerializedSeal{
-// 					seal1,
-// 				},
-// 				ParentCommittedSeal: &SerializedSeal{
-// 					seal2,
-// 				},
-// 			},
-// 		},
-// 	}
+	cases := []struct {
+		from *IstanbulExtra
+		to   *IstanbulExtra
+	}{
+		{
+			from: &IstanbulExtra{
+				Validators: &validators.ECDSAValidatorSet{
+					types.StringToAddress("1"),
+				},
+				Seal: seal1,
+				CommittedSeal: &SerializedSeal{
+					seal1,
+				},
+				ParentCommittedSeal: &SerializedSeal{
+					seal2,
+				},
+			},
+			to: &IstanbulExtra{
+				Validators:          &validators.ECDSAValidatorSet{},
+				Seal:                seal1,
+				CommittedSeal:       &SerializedSeal{},
+				ParentCommittedSeal: &SerializedSeal{},
+			},
+		},
+	}
 
-// 	for _, c := range cases {
-// 		data := c.data.MarshalRLPTo(nil)
+	for _, c := range cases {
+		data := c.from.MarshalRLPTo(nil)
 
-// 		ii := &IstanbulExtra{}
-// 		if err := ii.UnmarshalRLP(data); err != nil {
-// 			t.Fatal(err)
-// 		}
+		if err := c.to.UnmarshalRLP(data); err != nil {
+			t.Fatal(err)
+		}
 
-// 		if !reflect.DeepEqual(c.data, ii) {
-// 			t.Fatal("bad")
-// 		}
-// 	}
-// }
+		if !reflect.DeepEqual(c.from, c.to) {
+			t.Fatal("bad")
+		}
+	}
+}
 
-// func generateKeysAndAddresses(t *testing.T, num int) ([]*ecdsa.PrivateKey, []types.Address) {
-// 	t.Helper()
+func generateKeysAndAddresses(t *testing.T, num int) ([]*ecdsa.PrivateKey, []types.Address) {
+	t.Helper()
 
-// 	keys := make([]*ecdsa.PrivateKey, num)
-// 	addrs := make([]types.Address, num)
+	keys := make([]*ecdsa.PrivateKey, num)
+	addrs := make([]types.Address, num)
 
-// 	for i := range keys {
-// 		pk, addr := tests.GenerateKeyAndAddr(t)
-// 		keys[i] = pk
-// 		addrs[i] = addr
-// 	}
+	for i := range keys {
+		pk, addr := tests.GenerateKeyAndAddr(t)
+		keys[i] = pk
+		addrs[i] = addr
+	}
 
-// 	return keys, addrs
-// }
+	return keys, addrs
+}
 
-// func createIBFTHeader(
-// 	t *testing.T,
-// 	num uint64,
-// 	parentHash types.Hash,
-// 	validators []types.Address,
-// 	lastCommittedSeal [][]byte,
-// ) *types.Header {
-// 	t.Helper()
+func createIBFTHeader(
+	t *testing.T,
+	signer Signer,
+	num uint64,
+	parentHeader *types.Header,
+	validators validators.ValidatorSet,
+) *types.Header {
+	t.Helper()
 
-// 	header := &types.Header{
-// 		Number:     num,
-// 		ParentHash: parentHash,
-// 	}
+	header := &types.Header{
+		Number:     num,
+		ParentHash: parentHeader.Hash,
+	}
 
-// 	sSeal := SerializedSeal(lastCommittedSeal)
+	signer.InitIBFTExtra(header, parentHeader, validators)
 
-// 	initIbftExtra(header, validators, &sSeal, false)
-
-// 	return header
-// }
+	return header
+}
 
 // Test Scenario
 // 1. 4 IBFT Validators create headers
 // 2. A faulty node scans the past headers and appends new committed seal
 // 3. Check if each hash of the headers is wrong
-// func TestAppendCommittedSeal(t *testing.T) {
-// 	useIstanbulHeaderHash(t)
+func TestAppendECDSACommittedSeal(t *testing.T) {
+	var (
+		numHeaders          = 5
+		numNormalValidators = 4
+		numFaultyValidators = 1
 
-// 	var (
-// 		numHeaders          = 5
-// 		numNormalValidators = 4
-// 		numFaultyValidators = 1
+		headers                     = make([]*types.Header, 0, numHeaders)
+		faultyHeaders               = make([]*types.Header, 0, numHeaders)
+		parentHeader  *types.Header = &types.Header{}
 
-// 		headers                      = make([]*types.Header, 0, numHeaders)
-// 		faultyHeaders                = make([]*types.Header, 0, numHeaders)
-// 		parentHash                   = types.StringToHash("genesis")
-// 		parentCommittedSeal [][]byte = nil
+		keys, addresses     = generateKeysAndAddresses(t, numNormalValidators+numFaultyValidators)
+		normalValidatorKeys = keys[:numNormalValidators]
+		faultyValidatorKey  = keys[numNormalValidators]
 
-// 		keys, addresses     = generateKeysAndAddresses(t, numNormalValidators+numFaultyValidators)
-// 		normalValidatorKeys = keys[:numNormalValidators]
-// 		faultyValidatorKey  = keys[numNormalValidators]
+		signerA = &SignerImpl{
+			NewECDSAKeyManagerFromKey(keys[0]),
+		}
+		validators = validators.ECDSAValidatorSet(addresses)
 
-// 		err error
-// 	)
+		err error
+	)
 
-// 	// create headers by normal validators
-// 	for i := 0; i < numHeaders; i++ {
-// 		header := createIBFTHeader(t, uint64(i+1), parentHash, addresses, parentCommittedSeal)
+	useIstanbulHeaderHash(t, signerA)
 
-// 		// write seal
-// 		header, err = writeSeal(keys[0], header)
-// 		assert.NoError(t, err)
+	// create headers by normal validators
+	for i := 0; i < numHeaders; i++ {
+		header := createIBFTHeader(t, signerA, uint64(i+1), parentHeader, &validators)
 
-// 		// write committed seal
-// 		committedSeal := make([][]byte, len(normalValidatorKeys))
-// 		for i, key := range normalValidatorKeys {
-// 			committedSeal[i], err = createCommittedSeal(key, header)
-// 			assert.NoError(t, err)
-// 		}
+		// write seal
+		header, err = signerA.WriteSeal(header)
+		assert.NoError(t, err)
 
-// 		sSeal := SerializedSeal(committedSeal)
-// 		assert.NoError(t, packCommittedSealIntoIbftExtra(header, &sSeal))
+		// write committed seal
+		committedSeal := make(map[types.Address][]byte, len(normalValidatorKeys))
+		for _, key := range normalValidatorKeys {
+			signer := NewSigner(NewECDSAKeyManagerFromKey(key))
+			seal, err := signer.CreateCommittedSeal(header)
 
-// 		header = header.ComputeHash()
+			assert.NoError(t, err)
 
-// 		headers = append(headers, header)
+			committedSeal[crypto.PubKeyToAddress(&key.PublicKey)] = seal
+		}
 
-// 		parentHash = header.Hash
-// 		parentCommittedSeal = committedSeal
-// 	}
+		header, err = signerA.WriteCommittedSeals(header, committedSeal)
 
-// 	// faulty node scans the past headers and try to inject new committed seal
-// 	for i, h := range headers {
-// 		header := h.Copy()
+		assert.NoError(t, err)
 
-// 		// update parent hash & committed seal
-// 		if i > 0 {
-// 			parentHeader := faultyHeaders[i-1]
+		header = header.ComputeHash()
 
-// 			// update parent hash
-// 			header.ParentHash = parentHeader.Hash
+		headers = append(headers, header)
 
-// 			// get parent committed seal
-// 			x, err := unpackCommittedSealFromIbftExtra(parentHeader)
-// 			assert.NoError(t, err)
+		parentHeader = header
+	}
 
-// 			// update ParentCommittedSeal forcibly
-// 			err = packFieldIntoIbftExtra(header, func(extra *IstanbulExtra) {
-// 				extra.ParentCommittedSeal = x
-// 			})
-// 			assert.NoError(t, err)
-// 		}
+	// faulty node scans the past headers and try to inject new committed seal
+	for i, h := range headers {
+		header := h.Copy()
 
-// 		// create new committed seal
-// 		fx, err := createCommittedSeal(faultyValidatorKey, header)
-// 		assert.NoError(t, err)
+		// update parent hash & committed seal
+		if i > 0 {
+			parentHeader := faultyHeaders[i-1]
 
-// 		// append new committed seal
-// 		err = packFieldIntoIbftExtra(header, func(extra *IstanbulExtra) {
-// 			sseal := extra.CommittedSeal.(*SerializedSeal)
-// 			ssealSlice := [][]byte(*sseal)
+			// update parent hash
+			header.ParentHash = parentHeader.Hash
 
-// 			ssealSlice = append(ssealSlice, fx)
+			// get parent committed seal
+			extra, err := signerA.GetIBFTExtra(parentHeader)
+			assert.NoError(t, err)
 
-// 			nsseal := SerializedSeal(ssealSlice)
+			parentCommittedSeal := extra.ParentCommittedSeal
 
-// 			extra.CommittedSeal = &nsseal
-// 		})
-// 		assert.NoError(t, err)
+			// update ParentCommittedSeal forcibly
+			err = signerA.packFieldIntoIbftExtra(header, func(extra *IstanbulExtra) {
+				extra.ParentCommittedSeal = parentCommittedSeal
+			})
+			assert.NoError(t, err)
+		}
 
-// 		header = header.ComputeHash()
-// 		faultyHeaders = append(faultyHeaders, header)
-// 	}
+		// create new committed seal
+		faultySigner := NewSigner(NewECDSAKeyManagerFromKey(faultyValidatorKey))
+		fx, err := faultySigner.CreateCommittedSeal(header)
+		assert.NoError(t, err)
 
-// 	// Check hashes are different
-// 	for i := range headers {
-// 		header, faultyHeader := headers[i], faultyHeaders[i]
+		// append new committed seal
+		err = signerA.packFieldIntoIbftExtra(header, func(extra *IstanbulExtra) {
+			sseal := extra.CommittedSeal.(*SerializedSeal)
+			ssealSlice := [][]byte(*sseal)
 
-// 		if i == 0 {
-// 			// hashes should be same because first header doesn't have parent committed seal
-// 			assert.Equal(t, header.Hash, faultyHeader.Hash)
-// 		} else {
-// 			assert.NotEqual(t, header.Hash, faultyHeader.Hash)
-// 		}
-// 	}
-// }
+			ssealSlice = append(ssealSlice, fx)
+
+			nsseal := SerializedSeal(ssealSlice)
+
+			extra.CommittedSeal = &nsseal
+		})
+		assert.NoError(t, err)
+
+		header = header.ComputeHash()
+		faultyHeaders = append(faultyHeaders, header)
+	}
+
+	// Check hashes are different
+	for i := range headers {
+		header, faultyHeader := headers[i], faultyHeaders[i]
+
+		if i == 0 {
+			// hashes should be same because first header doesn't have parent committed seal
+			assert.Equal(t, header.Hash, faultyHeader.Hash)
+		} else {
+			assert.NotEqual(t, header.Hash, faultyHeader.Hash)
+		}
+	}
+}
