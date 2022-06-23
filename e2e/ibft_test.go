6317f5356973997eb32cd90796616d517b9f3fc5
kourin <kourin.code@gmail.com>
2022-06-23 12:22:55 +0300

    Fix lint error

--- e2e/ibft_test.go
+++ e2e/ibft_test.go
@@ -84,7 +84,11 @@ func TestIbft_TransactionFeeRecipient(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
+		tc := tc
+
 		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
 			senderKey, senderAddr := tests.GenerateKeyAndAddr(t)
 			_, receiverAddr := tests.GenerateKeyAndAddr(t)
 
