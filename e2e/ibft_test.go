bf5894df0fcf48b04a69ae60526d4c0d4d379573
kourin <kourin.code@gmail.com>
2022-06-24 11:45:37 +0300

    Merge IBFT e2e tests

--- e2e/ibft_test.go
+++ e2e/ibft_test.go
@@ -21,47 +21,83 @@ import (
 	and verifies it was mined
 **/
 func TestIbft_Transfer(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name            string
+		blockTime       uint64
+		ibftBaseTimeout uint64
+	}{
+		{
+			name:            "default block time",
+			blockTime:       config.DefaultBlockTime,
+			ibftBaseTimeout: 0, // use default value
+		},
+		{
+			name:            "longer block time",
+			blockTime:       10,
+			ibftBaseTimeout: 20,
+		},
+	}
+
 	var (
 		senderKey, senderAddr = tests.GenerateKeyAndAddr(t)
 		_, receiverAddr       = tests.GenerateKeyAndAddr(t)
 	)
 
-	ibftManager := framework.NewIBFTServersManager(t,
-		IBFTMinNodes,
-		IBFTDirPrefix,
-		func(i int, config *framework.TestServerConfig) {
-			config.Premine(senderAddr, framework.EthToWei(10))
-			config.SetSeal(true)
-		},
-	)
+	for _, tc := range testCases {
+		tc := tc
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 
-	ibftManager.StartServers(ctx)
+			ibftManager := framework.NewIBFTServersManager(t,
+				IBFTMinNodes,
+				IBFTDirPrefix,
+				func(i int, config *framework.TestServerConfig) {
+					config.Premine(senderAddr, framework.EthToWei(10))
+					config.SetSeal(true)
+					config.SetBlockTime(tc.blockTime)
+					config.SetIBFTBaseTimeout(tc.ibftBaseTimeout)
+				},
+			)
 
-	txn := &framework.PreparedTransaction{
-		From:     senderAddr,
-		To:       &receiverAddr,
-		GasPrice: big.NewInt(10000),
-		Gas:      1000000,
-		Value:    framework.EthToWei(1),
+			var (
+				startTimeout = time.Duration(tc.ibftBaseTimeout+20) * time.Second
+				txTimeout    = time.Duration(tc.ibftBaseTimeout+10) * time.Second
+			)
+
+			ctxForStart, cancelStart := context.WithTimeout(context.Background(), startTimeout)
+			defer cancelStart()
+
+			ibftManager.StartServers(ctxForStart)
+
+			txn := &framework.PreparedTransaction{
+				From:     senderAddr,
+				To:       &receiverAddr,
+				GasPrice: big.NewInt(10000),
+				Gas:      1000000,
+				Value:    framework.EthToWei(1),
+			}
+
+			ctxForTx, cancelTx := context.WithTimeout(context.Background(), txTimeout)
+			defer cancelTx()
+
+			//	send tx and wait for receipt
+			receipt, err := ibftManager.
+				GetServer(0).
+				SendRawTx(ctxForTx, txn, senderKey)
+
+			assert.NoError(t, err)
+			assert.NotNil(t, receipt)
+			assert.NotNil(t, receipt.TransactionHash)
+		})
 	}
-
-	ctx, cancel = context.WithTimeout(context.Background(), framework.DefaultTimeout)
-	defer cancel()
-
-	//	send tx and wait for receipt
-	receipt, err := ibftManager.
-		GetServer(0).
-		SendRawTx(ctx, txn, senderKey)
-
-	assert.NoError(t, err)
-	assert.NotNil(t, receipt)
-	assert.NotNil(t, receipt.TransactionHash)
 }
 
 func TestIbft_TransactionFeeRecipient(t *testing.T) {
+	t.Parallel()
+
 	testCases := []struct {
 		name         string
 		contractCall bool
@@ -156,31 +192,4 @@ func TestIbft_TransactionFeeRecipient(t *testing.T) {
 			assert.Equalf(t, txFee, balanceProposer, "Proposer didn't get appropriate transaction fee")
 		})
 	}
-}
-
-func TestIbft_Long_BlockGenerationTime(t *testing.T) {
-	// Set the longer value than default timeout into block generation time
-	blockTime := config.DefaultIBFTBaseTimeout + 10
-
-	// Change base timeout from 10 seconds to 30 seconds
-	ibftTimeout := blockTime + 10
-
-	ibftManager := framework.NewIBFTServersManager(
-		t,
-		IBFTMinNodes,
-		IBFTDirPrefix,
-		func(i int, config *framework.TestServerConfig) {
-			config.SetSeal(true)
-			config.SetBlockTime(blockTime)
-			config.SetIBFTBaseTimeout(ibftTimeout)
-			config.SetShowsLog(i == 0)
-		})
-
-	timeoutForBlockProduction := time.Duration(ibftTimeout+10) * time.Second
-	ctxForStart, cancelForStart := context.WithTimeout(context.Background(), timeoutForBlockProduction)
-
-	defer cancelForStart()
-
-	// StartServers waits until the first block is generated
-	ibftManager.StartServers(ctxForStart)
 }
