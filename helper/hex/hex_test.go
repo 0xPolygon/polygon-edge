70e04dfc8686d02178eb0868d3bf78e3f7d54f4f
AleksaOpacic <aleksaopacic988@gmail.com>
2022-07-19 17:44:09 +0200

    Fix lint

--- helper/hex/hex_test.go
+++ helper/hex/hex_test.go
@@ -7,22 +7,19 @@ import (
 
 const (
 	minUint64 uint64 = 0
-	maxUint64        = 18446744073709551615
+	maxUint64 uint64 = 18446744073709551615
 )
 
 func TestEncodingUint64(t *testing.T) {
-
 	// random uint64 array, min and max included
 	uint64Array := [10]uint64{minUint64, 30073, 67312, 71762, 11, 80604, 45403, 1, 298, maxUint64}
 
 	for _, value := range uint64Array {
-
 		encodedValue := EncodeUint64(value)
 
 		decodedValue, err := DecodeUint64(encodedValue)
 		assert.NoError(t, err)
 
 		assert.Equal(t, value, decodedValue)
-
 	}
 }
