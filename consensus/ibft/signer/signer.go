d8d787da07daf4f7c378e753aae7adbc5ed53c97
kourin <kourin.code@gmail.com>
2022-08-15 02:53:39 +0300

    Remove unused error

--- consensus/ibft/signer/signer.go
+++ consensus/ibft/signer/signer.go
@@ -21,7 +21,6 @@ var (
 	ErrValidatorNotFound          = errors.New("not found validator in validator set")
 	ErrInvalidValidatorSet        = errors.New("invalid validator set type")
 	ErrInvalidSignature           = errors.New("invalid signature")
-	ErrNilParentHeader            = errors.New("parent header is nil")
 )
 
 // Signer is responsible for signing for blocks and messages in IBFT
