d2f59d62cb14506a27767dd3735443f9b42bb8b1
AleksaOpacic <aleksaopacic988@gmail.com>
2022-08-22 22:39:30 +0200

    Make deployment result omitempty

--- command/whitelist/deployment/result.go
+++ command/whitelist/deployment/result.go
@@ -8,8 +8,8 @@ import (
 )
 
 type DeploymentResult struct {
-	AddAddress    []types.Address `json:"add_address"`
-	RemoveAddress []types.Address `json:"remove_address"`
+	AddAddress    []types.Address `json:"add_address,omitempty"`
+	RemoveAddress []types.Address `json:"remove_address,omitempty"`
 	Whitelist     []types.Address `json:"whitelist"`
 }
 
