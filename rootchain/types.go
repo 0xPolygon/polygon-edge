c8148c88c14c66aca33657fb10fca6fb30383a45
dbrajovic <dbrajovic3@gmail.com>
2022-09-05 12:03:27 +0200

    define basic types

---
+++ rootchain/types.go
@@ -0,0 +1,32 @@
+package rootchain
+
+//	TODO: define iota types
+type PayloadType uint8
+
+type Payload interface {
+	Get() (PayloadType, []byte)
+}
+
+type Event struct {
+	Number      uint64 // index of the event emitted from the rootchain contract
+	BlockNumber uint64 // number of the block the event was contained in
+
+	Payload // event specific data
+}
+
+type SAM struct {
+	Hash      []byte // unique hash of the event
+	Signature []byte // validator signature
+
+	Event
+}
+
+type VerifiedSAM []SAM
+
+func (v VerifiedSAM) Signatures() (signatures [][]byte) {
+	for _, m := range v {
+		signatures = append(signatures, m.Signature)
+	}
+
+	return
+}
