794aad3b01341ec52b2f7761a4cbd07be93f913e
dbrajovic <dbrajovic3@gmail.com>
2022-09-05 15:26:14 +0200

    add test

--- rootchain/sampool/pool_test.go
+++ rootchain/sampool/pool_test.go
@@ -42,6 +42,7 @@ func TestSAMPool_AddMessage(t *testing.T) {
 				verifyHash: func(sam rootchain.SAM) error {
 					return nil
 				},
+
 				verifySignature: func(sam rootchain.SAM) error {
 					return errors.New("some really bad signature")
 				},
@@ -51,6 +52,37 @@ func TestSAMPool_AddMessage(t *testing.T) {
 
 			msg := rootchain.SAM{
 				Signature: []byte("some really bad signature"),
+			}
+
+			err := pool.AddMessage(msg)
+
+			assert.Error(t, err)
+		},
+	)
+
+	t.Run(
+		"reject stale message",
+		func(t *testing.T) {
+			t.Parallel()
+
+			verfier := mockVerifier{
+				verifyHash: func(sam rootchain.SAM) error {
+					return nil
+				},
+				verifySignature: func(sam rootchain.SAM) error {
+					return nil
+				},
+				quorumFunc: nil,
+			}
+
+			pool := New(verfier)
+
+			pool.lastProcessedEvent = 10
+
+			msg := rootchain.SAM{
+				Event: rootchain.Event{
+					Number: 5,
+				},
 			}
 
 			err := pool.AddMessage(msg)
