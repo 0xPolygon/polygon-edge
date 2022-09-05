1d37b89680e3ac626c057cccb83ae617e3d0f24b
dbrajovic <dbrajovic3@gmail.com>
2022-09-05 13:03:38 +0200

    add hash verification in AddMessage

--- rootchain/sampool/pool.go
+++ rootchain/sampool/pool.go
@@ -2,21 +2,29 @@ package sampool
 
 import "github.com/0xPolygon/polygon-edge/rootchain"
 
-//	Callback used for determining if there is a valid
-//	number of SAM messages
-type QuorumFunc func(uint64) bool
-
 //	Verifies hash and signature of a SAM
 type Verifier interface {
-	VerifyHash(msg rootchain.SAM) error
-	VerifySignature(msg rootchain.SAM) error
+	VerifyHash(rootchain.SAM) error
+	VerifySignature(rootchain.SAM) error
+
+	Quorum(uint64) bool
 }
 
 type SAMPool struct {
+	verifier Verifier
+}
+
+func New(verifier Verifier) *SAMPool {
+	return &SAMPool{
+		verifier: verifier,
+	}
 }
 
 func (s *SAMPool) AddMessage(msg rootchain.SAM) error {
 	//	verify message hash
+	if err := s.verifier.VerifyHash(msg); err != nil {
+		return err
+	}
 
 	//	verify message signature
 
