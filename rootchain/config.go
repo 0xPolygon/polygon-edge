3db04859f5ca472211a7d16f46786ddf3ddc6071
Miloš Živković <milos.zivkovic@mvpworkshop.co>
2022-09-07 10:34:05 +0200

    [Rootchain Staking] Add rootchain config command (#716)
    * Add rootchain config struct & test
    
    * Add the rootchain command
    
    * Resolve linting error
    
    * Add block confirmations to the config
    
    * Add the rootchain.json to the gitignore
    
    * Bump the default number of block confirmations

---
+++ rootchain/config.go
@@ -0,0 +1,16 @@
+package rootchain
+
+// Config defines the rootchain config mapping "0xa... (rootchain address)" => []ConfigEvent
+type Config struct {
+	RootchainAddresses map[string][]ConfigEvent `json:"rootchainAddresses"`
+}
+
+// ConfigEvent defines a single event that needs to be listened for on the rootchain,
+// and corresponding local metadata
+type ConfigEvent struct {
+	EventABI           string      `json:"eventABI"`
+	MethodABI          string      `json:"methodABI"`
+	LocalAddress       string      `json:"localAddress"`
+	PayloadType        PayloadType `json:"payloadType"`
+	BlockConfirmations uint64      `json:"blockConfirmations"`
+}
---
+++ rootchain/config_test.go
@@ -0,0 +1,49 @@
+package rootchain
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRootchainConfig(t *testing.T) {
+	const (
+		rootchainAddress   = "0x123456"
+		eventABI           = "event ABI"
+		methodABI          = "method ABI"
+		localAddress       = "0x123123"
+		payloadType        = ValidatorSetPayload
+		blockConfirmations = uint64(10)
+	)
+
+	config := Config{
+		RootchainAddresses: map[string][]ConfigEvent{
+			rootchainAddress: {
+				{
+					EventABI:           eventABI,
+					MethodABI:          methodABI,
+					LocalAddress:       localAddress,
+					PayloadType:        payloadType,
+					BlockConfirmations: blockConfirmations,
+				},
+			},
+		},
+	}
+
+	// Marshal the data into JSON
+	marshalledData, err := json.Marshal(config)
+	if err != nil {
+		t.Fatalf("unable to marshal rootchain config, %v", err)
+	}
+
+	// Unmarshal the data
+	destination := reflect.New(reflect.TypeOf(config))
+	if err = json.Unmarshal(marshalledData, destination.Interface()); err != nil {
+		t.Fatalf("unable to unmarshal rootchain config, %v", err)
+	}
+
+	// Make sure the configs are equal
+	assert.Equal(t, config, destination.Elem().Interface())
+}
