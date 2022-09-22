ecb0a096f1440d3e314edf65744192128f4244eb
dbrajovic <dbrajovic3@gmail.com>
2022-09-22 13:44:56 +0200

    Revert "Merge branch 'release/0.5.1' into pos/custom-contracts"
    This reverts commit a55290c59e7a5edb95438c7a693ddf28cfb1acec, reversing
    changes made to cf93780a87a191a7ac1cd22d9d579ee923703a49.

--- consensus/ibft/ibft.go
+++ consensus/ibft/ibft.go
@@ -259,10 +259,8 @@ func (i *backendIBFT) startConsensus() {
 	// to insert a valid block. Used for cancelling active consensus
 	// rounds for a specific height
 	go func() {
-		eventCh := newBlockSub.GetEventCh()
-
 		for {
-			if ev := <-eventCh; ev.Source == "syncer" {
+			if ev := <-newBlockSub.GetEventCh(); ev.Source == "syncer" {
 				if ev.NewChain[0].Number < i.blockchain.Header().Number {
 					// The blockchain notification system can eventually deliver
 					// stale block notifications. These should be ignored
