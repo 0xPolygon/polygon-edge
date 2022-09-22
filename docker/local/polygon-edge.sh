ca753daaf923bbcbc8563190ee0dba009cbeb415
epikichi <ji@epik.dev>
2022-09-22 09:12:06 -0400

    Adding switching volumes mount target for genesis

--- docker/local/polygon-edge.sh
+++ docker/local/polygon-edge.sh
@@ -8,14 +8,14 @@ case "$1" in
 
    "init")
       echo "Generating secrets..."
-      node1id=$("$POLYGON_EDGE_BIN" secrets init --data-dir /data1 | grep Node | awk -F ' ' '{print $4}')
-      node2id=$("$POLYGON_EDGE_BIN" secrets init --data-dir /data2 | grep Node | awk -F ' ' '{print $4}')
-      "$POLYGON_EDGE_BIN" secrets init --data-dir /data3 | grep Node | awk -F ' ' '{print $4}'
-      "$POLYGON_EDGE_BIN" secrets init --data-dir /data4 | grep Node | awk -F ' ' '{print $4}'
+      node1id=$("$POLYGON_EDGE_BIN" secrets init --data-dir data-1 | grep Node | awk -F ' ' '{print $4}')
+      node2id=$("$POLYGON_EDGE_BIN" secrets init --data-dir data-2 | grep Node | awk -F ' ' '{print $4}')
+      "$POLYGON_EDGE_BIN" secrets init --data-dir data-3 | grep Node | awk -F ' ' '{print $4}'
+      "$POLYGON_EDGE_BIN" secrets init --data-dir data-4 | grep Node | awk -F ' ' '{print $4}'
       echo "Secrets have been successfully generated"
 
       echo "Generating genesis file..."
-      "$POLYGON_EDGE_BIN" genesis --dir /genesis/genesis.json --consensus ibft --ibft-validators-prefix-path data --bootnode /ip4/127.0.0.1/tcp/10001/p2p/"$node1id" --bootnode /ip4/127.0.0.1/tcp/20001/p2p/"$node2id"
+      "$POLYGON_EDGE_BIN" genesis --dir /genesis/genesis.json --consensus ibft --ibft-validators-prefix-path data- --bootnode /ip4/127.0.0.1/tcp/10001/p2p/"$node1id" --bootnode /ip4/127.0.0.1/tcp/20001/p2p/"$node2id"
       echo "Genesis file has been successfully generated"
       ;;
 
