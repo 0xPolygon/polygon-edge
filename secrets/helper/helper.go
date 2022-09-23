d592677ff4a8977729607633063b13545f957bfb
epikichi <ji@epik.dev>
2022-09-23 05:16:30 -0400

    fmt

--- secrets/helper/helper.go
+++ secrets/helper/helper.go
@@ -14,8 +14,8 @@ import (
 	"github.com/0xPolygon/polygon-edge/secrets/local"
 	"github.com/0xPolygon/polygon-edge/types"
 	"github.com/hashicorp/go-hclog"
-	"github.com/libp2p/go-libp2p/core/peer"
 	libp2pCrypto "github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
 )
 
 // SetupLocalSecretsManager is a helper method for boilerplate local secrets manager setup
