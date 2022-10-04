86bf55d35577007df6902422c99d7cbfacb1ff07
Stefan Negovanović <stefan@ethernal.tech>
2022-10-04 10:10:49 +0200

    Add pbft genesis command

---
+++ command/pbft_genesis/params.go
@@ -0,0 +1,349 @@
+package pbft_genesis
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/0xPolygon/polygon-edge/chain"
+	"github.com/0xPolygon/polygon-edge/command"
+	"github.com/0xPolygon/polygon-edge/command/helper"
+	"github.com/0xPolygon/polygon-edge/consensus/ibft"
+	"github.com/0xPolygon/polygon-edge/consensus/ibft/fork"
+	"github.com/0xPolygon/polygon-edge/consensus/ibft/signer"
+	"github.com/0xPolygon/polygon-edge/contracts/staking"
+	stakingHelper "github.com/0xPolygon/polygon-edge/helper/staking"
+	"github.com/0xPolygon/polygon-edge/server"
+	"github.com/0xPolygon/polygon-edge/types"
+	"github.com/0xPolygon/polygon-edge/validators"
+)
+
+const (
+	dirFlag           = "dir"
+	nameFlag          = "name"
+	premineFlag       = "premine"
+	chainIDFlag       = "chain-id"
+	epochSizeFlag     = "epoch-size"
+	blockGasLimitFlag = "block-gas-limit"
+	posFlag           = "pos"
+	minValidatorCount = "min-validator-count"
+	maxValidatorCount = "max-validator-count"
+)
+
+// Legacy flags that need to be preserved for running clients
+const (
+	chainIDFlagLEGACY = "chainid"
+)
+
+var (
+	params = &genesisParams{}
+)
+
+var (
+	errValidatorsNotSpecified = errors.New("validator information not specified")
+	errUnsupportedConsensus   = errors.New("specified consensusRaw not supported")
+	errInvalidEpochSize       = errors.New("epoch size must be greater than 1")
+)
+
+type genesisParams struct {
+	genesisPath         string
+	name                string
+	consensusRaw        string
+	validatorPrefixPath string
+	premine             []string
+	bootnodes           []string
+	ibftValidators      validators.Validators
+
+	ibftValidatorsRaw []string
+
+	chainID       uint64
+	epochSize     uint64
+	blockGasLimit uint64
+	isPos         bool
+
+	minNumValidators uint64
+	maxNumValidators uint64
+
+	rawIBFTValidatorType string
+	ibftValidatorType    validators.ValidatorType
+
+	extraData []byte
+	consensus server.ConsensusType
+
+	consensusEngineConfig map[string]interface{}
+
+	genesisConfig *chain.Chain
+}
+
+func (p *genesisParams) validateFlags() error {
+	// Check if the consensusRaw is supported
+	if !server.ConsensusSupported(p.consensusRaw) {
+		return errUnsupportedConsensus
+	}
+
+	// Check if validator information is set at all
+	if p.isIBFTConsensus() &&
+		!p.areValidatorsSetManually() &&
+		!p.areValidatorsSetByPrefix() {
+		return errValidatorsNotSpecified
+	}
+
+	// Check if the genesis file already exists
+	if generateError := verifyGenesisExistence(p.genesisPath); generateError != nil {
+		return errors.New(generateError.GetMessage())
+	}
+
+	// Check that the epoch size is correct
+	if p.epochSize < 2 && server.ConsensusType(p.consensusRaw) == server.IBFTConsensus {
+		// Epoch size must be greater than 1, so new transactions have a chance to be added to a block.
+		// Otherwise, every block would be an endblock (meaning it will not have any transactions).
+		// Check is placed here to avoid additional parsing if epochSize < 2
+		return errInvalidEpochSize
+	}
+
+	// Validate min and max validators number
+	if err := command.ValidateMinMaxValidatorsNumber(p.minNumValidators, p.maxNumValidators); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *genesisParams) isIBFTConsensus() bool {
+	return server.ConsensusType(p.consensusRaw) == server.IBFTConsensus
+}
+
+func (p *genesisParams) areValidatorsSetManually() bool {
+	return len(p.ibftValidatorsRaw) != 0
+}
+
+func (p *genesisParams) areValidatorsSetByPrefix() bool {
+	return p.validatorPrefixPath != ""
+}
+
+func (p *genesisParams) getRequiredFlags() []string {
+	return []string{
+		command.BootnodeFlag,
+	}
+}
+
+func (p *genesisParams) initRawParams() error {
+	p.consensus = server.ConsensusType(p.consensusRaw)
+
+	if err := p.initIBFTValidatorType(); err != nil {
+		return err
+	}
+
+	if err := p.initValidatorSet(); err != nil {
+		return err
+	}
+
+	p.initIBFTExtraData()
+	p.initConsensusEngineConfig()
+
+	return nil
+}
+
+// setValidatorSetFromCli sets validator set from cli command
+func (p *genesisParams) setValidatorSetFromCli() error {
+	if len(p.ibftValidatorsRaw) == 0 {
+		return nil
+	}
+
+	newValidators, err := validators.ParseValidators(p.ibftValidatorType, p.ibftValidatorsRaw)
+	if err != nil {
+		return err
+	}
+
+	if err = p.ibftValidators.Merge(newValidators); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// setValidatorSetFromPrefixPath sets validator set from prefix path
+func (p *genesisParams) setValidatorSetFromPrefixPath() error {
+	if !p.areValidatorsSetByPrefix() {
+		return nil
+	}
+
+	validators, err := command.GetValidatorsFromPrefixPath(
+		p.validatorPrefixPath,
+		p.ibftValidatorType,
+	)
+
+	if err != nil {
+		return fmt.Errorf("failed to read from prefix: %w", err)
+	}
+
+	if err := p.ibftValidators.Merge(validators); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *genesisParams) initIBFTValidatorType() error {
+	var err error
+	if p.ibftValidatorType, err = validators.ParseValidatorType(p.rawIBFTValidatorType); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *genesisParams) initValidatorSet() error {
+	p.ibftValidators = validators.NewValidatorSetFromType(p.ibftValidatorType)
+
+	// Set validator set
+	// Priority goes to cli command over prefix path
+	if err := p.setValidatorSetFromPrefixPath(); err != nil {
+		return err
+	}
+
+	if err := p.setValidatorSetFromCli(); err != nil {
+		return err
+	}
+
+	// Validate if validator number exceeds max number
+	if ok := p.isValidatorNumberValid(); !ok {
+		return command.ErrValidatorNumberExceedsMax
+	}
+
+	return nil
+}
+
+func (p *genesisParams) isValidatorNumberValid() bool {
+	return p.ibftValidators == nil || uint64(p.ibftValidators.Len()) <= p.maxNumValidators
+}
+
+func (p *genesisParams) initIBFTExtraData() {
+	if p.consensus != server.IBFTConsensus {
+		return
+	}
+
+	var committedSeal signer.Seals
+
+	switch p.ibftValidatorType {
+	case validators.ECDSAValidatorType:
+		committedSeal = new(signer.SerializedSeal)
+	case validators.BLSValidatorType:
+		committedSeal = new(signer.AggregatedSeal)
+	}
+
+	ibftExtra := &signer.IstanbulExtra{
+		Validators:     p.ibftValidators,
+		ProposerSeal:   []byte{},
+		CommittedSeals: committedSeal,
+	}
+
+	p.extraData = make([]byte, signer.IstanbulExtraVanity)
+	p.extraData = ibftExtra.MarshalRLPTo(p.extraData)
+}
+
+func (p *genesisParams) initConsensusEngineConfig() {
+	if p.consensus != server.IBFTConsensus {
+		p.consensusEngineConfig = map[string]interface{}{
+			p.consensusRaw: map[string]interface{}{},
+		}
+
+		return
+	}
+
+	if p.isPos {
+		p.initIBFTEngineMap(fork.PoS)
+
+		return
+	}
+
+	p.initIBFTEngineMap(fork.PoA)
+}
+
+func (p *genesisParams) initIBFTEngineMap(ibftType fork.IBFTType) {
+	p.consensusEngineConfig = map[string]interface{}{
+		string(server.IBFTConsensus): map[string]interface{}{
+			fork.KeyType:          ibftType,
+			fork.KeyValidatorType: p.ibftValidatorType,
+			ibft.KeyEpochSize:     p.epochSize,
+		},
+	}
+}
+
+func (p *genesisParams) generateGenesis() error {
+	if err := p.initGenesisConfig(); err != nil {
+		return err
+	}
+
+	if err := helper.WriteGenesisConfigToDisk(
+		p.genesisConfig,
+		p.genesisPath,
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *genesisParams) initGenesisConfig() error {
+	chainConfig := &chain.Chain{
+		Name: p.name,
+		Genesis: &chain.Genesis{
+			GasLimit:   p.blockGasLimit,
+			Difficulty: 1,
+			Alloc:      map[types.Address]*chain.GenesisAccount{},
+			ExtraData:  p.extraData,
+			GasUsed:    command.DefaultGenesisGasUsed,
+		},
+		Params: &chain.Params{
+			ChainID: int(p.chainID),
+			Forks:   chain.AllForksEnabled,
+			Engine:  p.consensusEngineConfig,
+		},
+		Bootnodes: p.bootnodes,
+	}
+
+	// Predeploy staking smart contract if needed
+	if p.shouldPredeployStakingSC() {
+		stakingAccount, err := p.predeployStakingSC()
+		if err != nil {
+			return err
+		}
+
+		chainConfig.Genesis.Alloc[staking.AddrStakingContract] = stakingAccount
+	}
+
+	// Premine accounts
+	if err := fillPremineMap(chainConfig.Genesis.Alloc, p.premine); err != nil {
+		return err
+	}
+
+	p.genesisConfig = chainConfig
+
+	return nil
+}
+
+func (p *genesisParams) shouldPredeployStakingSC() bool {
+	// If the consensus selected is IBFT / Dev and the mechanism is Proof of Stake,
+	// deploy the Staking SC
+	return p.isPos && (p.consensus == server.IBFTConsensus || p.consensus == server.DevConsensus)
+}
+
+func (p *genesisParams) predeployStakingSC() (*chain.GenesisAccount, error) {
+	stakingAccount, predeployErr := stakingHelper.PredeployStakingSC(
+		p.ibftValidators,
+		stakingHelper.PredeployParams{
+			MinValidatorCount: p.minNumValidators,
+			MaxValidatorCount: p.maxNumValidators,
+		})
+	if predeployErr != nil {
+		return nil, predeployErr
+	}
+
+	return stakingAccount, nil
+}
+
+func (p *genesisParams) getResult() command.CommandResult {
+	return &GenesisResult{
+		Message: fmt.Sprintf("Genesis written to %s\n", p.genesisPath),
+	}
+}
---
+++ command/pbft_genesis/utils.go
@@ -0,0 +1,83 @@
+package pbft_genesis
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/0xPolygon/polygon-edge/chain"
+	"github.com/0xPolygon/polygon-edge/command"
+	"github.com/0xPolygon/polygon-edge/types"
+)
+
+const (
+	StatError   = "StatError"
+	ExistsError = "ExistsError"
+)
+
+// GenesisGenError is a specific error type for generating genesis
+type GenesisGenError struct {
+	message   string
+	errorType string
+}
+
+// GetMessage returns the message of the genesis generation error
+func (g *GenesisGenError) GetMessage() string {
+	return g.message
+}
+
+// GetType returns the type of the genesis generation error
+func (g *GenesisGenError) GetType() string {
+	return g.errorType
+}
+
+// verifyGenesisExistence checks if the genesis file at the specified path is present
+func verifyGenesisExistence(genesisPath string) *GenesisGenError {
+	_, err := os.Stat(genesisPath)
+	if err != nil && !os.IsNotExist(err) {
+		return &GenesisGenError{
+			message:   fmt.Sprintf("failed to stat (%s): %v", genesisPath, err),
+			errorType: StatError,
+		}
+	}
+
+	if !os.IsNotExist(err) {
+		return &GenesisGenError{
+			message:   fmt.Sprintf("genesis file at path (%s) already exists", genesisPath),
+			errorType: ExistsError,
+		}
+	}
+
+	return nil
+}
+
+// fillPremineMap fills the premine map for the genesis.json file with passed in balances and accounts
+func fillPremineMap(
+	premineMap map[types.Address]*chain.GenesisAccount,
+	premine []string,
+) error {
+	for _, prem := range premine {
+		var addr types.Address
+
+		val := command.DefaultPremineBalance
+
+		if indx := strings.Index(prem, ":"); indx != -1 {
+			// <addr>:<balance>
+			addr, val = types.StringToAddress(prem[:indx]), prem[indx+1:]
+		} else {
+			// <addr>
+			addr = types.StringToAddress(prem)
+		}
+
+		amount, err := types.ParseUint256orHex(&val)
+		if err != nil {
+			return fmt.Errorf("failed to parse amount %s: %w", val, err)
+		}
+
+		premineMap[addr] = &chain.GenesisAccount{
+			Balance: amount,
+		}
+	}
+
+	return nil
+}
