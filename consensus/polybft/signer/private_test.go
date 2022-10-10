3c8182b3fdb2b75b1e5ea44216de4adee7de8452
Stefan Negovanović <stefan@ethernal.tech>
2022-10-10 14:50:57 +0200

    Uncomment Test_PrivateMarshal

--- consensus/polybft/signer/private_test.go
+++ consensus/polybft/signer/private_test.go
@@ -1,23 +1,22 @@
 package bls
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_PrivateMarshal(t *testing.T) {
-	// blsKey, err := GenerateBlsKey() // structure which holds private/public key pair
-	// require.NoError(t, err)
-	//
-	// // marshal public key
-	// privateKeyMarshalled, err := blsKey.MarshalJSON()
-	// require.NoError(t, err)
-	// // recover private and public key
-	// blsKeyUnmarshalled, err := UnmarshalPrivateKey(privateKeyMarshalled)
-	// require.NoError(t, err)
-	//
-	// assert.Equal(t, blsKey, blsKeyUnmarshalled)
-	var b []byte
+	blsKey, err := GenerateBlsKey() // structure which holds private/public key pair
+	require.NoError(t, err)
 
-	fmt.Println(len(b))
+	// marshal public key
+	privateKeyMarshalled, err := blsKey.MarshalJSON()
+	require.NoError(t, err)
+	// recover private and public key
+	blsKeyUnmarshalled, err := UnmarshalPrivateKey(privateKeyMarshalled)
+	require.NoError(t, err)
+
+	assert.Equal(t, blsKey, blsKeyUnmarshalled)
 }
