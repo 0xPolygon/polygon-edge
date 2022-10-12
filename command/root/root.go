e27dca146149ee8ba7417df75506293087766ec7
Victor Castell <victor@victorcastell.com>
2022-10-12 16:43:48 +0200

    Move polybft-secrets command to command package

--- command/root/root.go
+++ command/root/root.go
@@ -12,13 +12,13 @@ import (
 	"github.com/0xPolygon/polygon-edge/command/loadbot"
 	"github.com/0xPolygon/polygon-edge/command/monitor"
 	"github.com/0xPolygon/polygon-edge/command/peers"
+	"github.com/0xPolygon/polygon-edge/command/polybftsecrets"
 	"github.com/0xPolygon/polygon-edge/command/secrets"
 	"github.com/0xPolygon/polygon-edge/command/server"
 	"github.com/0xPolygon/polygon-edge/command/status"
 	"github.com/0xPolygon/polygon-edge/command/txpool"
 	"github.com/0xPolygon/polygon-edge/command/version"
 	"github.com/0xPolygon/polygon-edge/command/whitelist"
-	"github.com/0xPolygon/polygon-edge/consensus/polybft/command/polybftsecrets"
 	"github.com/spf13/cobra"
 )
 
