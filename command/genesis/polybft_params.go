62a4f8c9349a80227591d43a7ccd43575e7463a9
Nemanja0x <nemanja@ethernal.tech>
2022-10-12 09:28:57 +0200

    ethgo.HexToAddress -> types.StringToAddress

--- command/genesis/polybft_params.go
+++ command/genesis/polybft_params.go
@@ -35,10 +35,10 @@ const (
 
 var (
 	// hard code address for the sidechain (this is only being used for testing)
-	ValidatorSetAddr         = ethgo.HexToAddress("0xBd770416a3345F91E4B34576cb804a576fa48EB1")
-	SidechainBridgeAddr      = ethgo.HexToAddress("0x5a443704dd4B594B382c22a083e2BD3090A6feF3")
-	sidechainERC20Addr       = ethgo.HexToAddress("0x47e9Fbef8C83A1714F1951F142132E6e90F5fa5D")
-	SidechainERC20BridgeAddr = ethgo.HexToAddress("0x8Be503bcdEd90ED42Eff31f56199399B2b0154CA")
+	ValidatorSetAddr         = types.StringToAddress("0xBd770416a3345F91E4B34576cb804a576fa48EB1")
+	SidechainBridgeAddr      = types.StringToAddress("0x5a443704dd4B594B382c22a083e2BD3090A6feF3")
+	sidechainERC20Addr       = types.StringToAddress("0x47e9Fbef8C83A1714F1951F142132E6e90F5fa5D")
+	SidechainERC20BridgeAddr = types.StringToAddress("0x8Be503bcdEd90ED42Eff31f56199399B2b0154CA")
 )
 
 func (p *genesisParams) getPolyBftConfig(validators []*polybft.Validator) (*polybft.PolyBFTConfig, error) {
