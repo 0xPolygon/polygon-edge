dd8ea9b8b5a38c40dd63dfc9bd0774138ff66412
Goran Rojovic <goran.rojovic@ethernal.tech>
2022-10-14 11:38:11 +0200

    CommitTransaction in blockBuilder

--- consensus/polybft/block_builder.go
+++ consensus/polybft/block_builder.go
@@ -189,22 +189,32 @@ write:
 	return nil
 }
 
+// CommitTransaction applies given transaction to the state. If transaction apply fails, it reverts the saved snapshot.
+func (b *BlockBuilder) CommitTransaction(tx *types.Transaction) error {
+	if tx.ExceedsBlockGasLimit(b.params.GasLimit) {
+		if err := b.state.WriteFailedReceipt(tx); err != nil {
+			return err
+		}
+
+		return txpool.ErrBlockLimitExceeded
+	}
+
+	if err := b.state.Write(tx); err != nil {
+		return err
+	}
+
+	b.txns = append(b.txns, tx)
+
+	return nil
+}
+
 func (b *BlockBuilder) writeTxPoolTransaction(tx *types.Transaction) (bool, error) {
 	if tx == nil {
 		return true, nil
 	}
 
-	if tx.ExceedsBlockGasLimit(b.params.GasLimit) {
-		b.params.TxPool.Drop(tx)
-
-		if err := b.state.WriteFailedReceipt(tx); err != nil {
-			return false, err
-		}
-
-		return false, txpool.ErrBlockLimitExceeded
-	}
-
-	if err := b.state.Write(tx); err != nil {
+	err := b.CommitTransaction(tx)
+	if err != nil {
 		if _, ok := err.(*state.GasLimitReachedTransitionApplicationError); ok { //nolint:errorlint
 			// stop processing
 			return true, err
@@ -221,7 +231,6 @@ func (b *BlockBuilder) writeTxPoolTransaction(tx *types.Transaction) (bool, erro
 
 	// remove tx from the pool and add it to the list of all block transactions
 	b.params.TxPool.Pop(tx)
-	b.txns = append(b.txns, tx)
 
 	return false, nil
 }
--- consensus/polybft/fsm.go
+++ consensus/polybft/fsm.go
@@ -25,6 +25,7 @@ type blockBuilder interface {
 	Fill() error
 	Build(func(h *types.Header)) (*StateBlock, error)
 	GetState() *state.Transition
+	CommitTransaction(tx *types.Transaction) error
 }
 
 const maxBundlesPerSprint = 50
