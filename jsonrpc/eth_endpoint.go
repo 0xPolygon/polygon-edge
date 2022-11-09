1096a403be7412875178ae5649c8800117c762ea
Ferran Borreguero <ferranbt@protonmail.com>
2022-11-09 14:24:32 +0100

    Fix comment

--- jsonrpc/eth_endpoint.go
+++ jsonrpc/eth_endpoint.go
@@ -559,7 +559,6 @@ func (e *Eth) EstimateGas(arg *txnArgs, rawNum *BlockNumber) (interface{}, error
 	// Checks if executor level valid gas errors occurred
 	isGasApplyError := func(err error) bool {
 		// Not linting this as the underlying error is actually wrapped
-		//nolint:govet
 		return errors.Is(err, state.ErrNotEnoughIntrinsicGas)
 	}
 
