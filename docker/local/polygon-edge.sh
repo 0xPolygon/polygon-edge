c9280bbb1c61a3651fd3cf23b39200d1d7c0a54a
Médi-Rémi Hashim <mediremi.hashim@antithesis.com>
2022-11-22 16:06:09 +0000

    Docker setup: don't run init if genesis file already exists

--- docker/local/polygon-edge.sh
+++ docker/local/polygon-edge.sh
@@ -8,18 +8,22 @@ GENESIS_PATH=/genesis/genesis.json
 case "$1" in
 
    "init")
-      echo "Generating secrets..."
-      secrets=$("$POLYGON_EDGE_BIN" secrets init --num 4 --data-dir data- --json)
-      echo "Secrets have been successfully generated"
+      if [ -f "$GENESIS_PATH" ]; then
+          echo "Secrets have already been generated."
+      else
+          echo "Generating secrets..."
+          secrets=$("$POLYGON_EDGE_BIN" secrets init --num 4 --data-dir data- --json)
+          echo "Secrets have been successfully generated"
 
-      echo "Generating genesis file..."
-      "$POLYGON_EDGE_BIN" genesis \
-        --dir "$GENESIS_PATH" \
-        --consensus ibft \
-        --ibft-validators-prefix-path data- \
-        --bootnode /dns4/node-1/tcp/1478/p2p/$(echo $secrets | jq -r '.[0] | .node_id') \
-        --bootnode /dns4/node-2/tcp/1478/p2p/$(echo $secrets | jq -r '.[1] | .node_id')
-      echo "Genesis file has been successfully generated"
+          echo "Generating genesis file..."
+          "$POLYGON_EDGE_BIN" genesis \
+            --dir "$GENESIS_PATH" \
+            --consensus ibft \
+            --ibft-validators-prefix-path data- \
+            --bootnode /dns4/node-1/tcp/1478/p2p/$(echo $secrets | jq -r '.[0] | .node_id') \
+            --bootnode /dns4/node-2/tcp/1478/p2p/$(echo $secrets | jq -r '.[1] | .node_id')
+          echo "Genesis file has been successfully generated"
+      fi
       ;;
 
    *)
