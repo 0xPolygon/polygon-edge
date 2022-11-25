92b74591cf89c9586619cdb6a7ab698cdafa1e42
Victor Castell <victor@victorcastell.com>
2022-11-25 17:22:54 +0100

    E2E test executing state syncs

--- e2e-polybft/bridge_test.go
+++ e2e-polybft/bridge_test.go
@@ -6,14 +6,15 @@ import (
 	"testing"
 	"time"
 
-	"github.com/0xPolygon/polygon-edge/consensus/polybft/wallet"
 	"github.com/0xPolygon/polygon-edge/contracts"
 	"github.com/0xPolygon/polygon-edge/e2e-polybft/framework"
 	"github.com/0xPolygon/polygon-edge/types"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/umbracle/ethgo"
 	"github.com/umbracle/ethgo/abi"
+	ethgow "github.com/umbracle/ethgo/wallet"
 )
 
 var stateSyncResultEvent = abi.MustNewEvent(`event StateSyncResult(
@@ -56,12 +57,14 @@ func TestE2E_Bridge_MainWorkflow(t *testing.T) {
 	const num = 10
 
 	var (
+		accounts         = make([]ethgo.Key, num)
 		wallets, amounts [num]string
 		premine          [num]types.Address
 	)
 
 	for i := 0; i < num; i++ {
-		premine[i] = types.Address(wallet.GenerateAccount().Ecdsa.Address())
+		accounts[i], _ = ethgow.GenerateKey()
+		premine[i] = types.Address(accounts[i].Address())
 		wallets[i] = premine[i].String()
 		amounts[i] = fmt.Sprintf("%d", 100)
 	}
@@ -83,7 +86,47 @@ func TestE2E_Bridge_MainWorkflow(t *testing.T) {
 	)
 
 	// wait for a few more sprints
-	require.NoError(t, cluster.WaitForBlock(40, 2*time.Minute))
+	require.NoError(t, cluster.WaitForBlock(25, 1*time.Minute))
+
+	// commitments should've been stored
+	var stateSyncProof types.StateSyncProof
+
+	// retrieve state sync proofs
+	err := cluster.Servers[0].JSONRPC().Call("bridge_getStateSyncProof", &stateSyncProof, "1")
+	require.NoError(t, err)
+
+	t.Log(stateSyncProof)
+
+	// execute the state sync
+	rawTxn := &ethgo.Transaction{
+		From:     ethgo.Address(premine[0]),
+		To:       (*ethgo.Address)(&contracts.StateReceiverContract),
+		GasPrice: 0,
+		Gas:      types.StateTransactionGasLimit,
+	}
+
+	chID, err := cluster.Servers[0].JSONRPC().Eth().ChainID()
+	require.NoError(t, err)
+
+	signer := ethgow.NewEIP155Signer(chID.Uint64())
+	signedTxn, err := signer.SignTx(rawTxn, accounts[0])
+	assert.NoError(t, err)
+
+	txnRaw, err := signedTxn.MarshalRLPTo(nil)
+	assert.NoError(t, err)
+
+	hash, err := cluster.Servers[0].JSONRPC().Eth().SendRawTransaction(txnRaw)
+	assert.NoError(t, err)
+
+	var receipt *ethgo.Receipt
+	for count := 0; count < 100 || receipt == nil; count++ {
+		receipt, err = cluster.Servers[0].JSONRPC().Eth().GetTransactionReceipt(hash)
+		assert.NoError(t, err)
+
+		time.Sleep(50 * time.Millisecond)
+	}
+	require.NotNil(t, receipt)
+	t.Log(receipt.Logs)
 
 	// the transaction is mined and there should be a success event
 	id := stateSyncResultEvent.ID()
