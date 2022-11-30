a07ae1a9587824488cac21aa63a5342add0dcf51
Stefan Negovanović <stefan@ethernal.tech>
2022-11-30 09:05:21 +0100

    Revert helper bash scripts

--- start-cluster.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-go run . server --data-dir ./test-chain-1 --chain genesis.json --grpc-address :5001 --libp2p :30301 --jsonrpc :9545 --seal --log-level DEBUG &
-go run . server --data-dir ./test-chain-2 --chain genesis.json --grpc-address :5002 --libp2p :30302 --jsonrpc :10002 --seal --log-level DEBUG &
-go run . server --data-dir ./test-chain-3 --chain genesis.json --grpc-address :5003 --libp2p :30303 --jsonrpc :10003 --seal --log-level DEBUG &
-go run . server --data-dir ./test-chain-4 --chain genesis.json --grpc-address :5004 --libp2p :30304 --jsonrpc :10004 --seal --log-level DEBUG
