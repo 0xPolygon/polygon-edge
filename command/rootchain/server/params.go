b5ff739d794b932138b0689b9c935dc215577877
Ferran <ferranbt@protonmail.com>
2022-12-05 19:27:43 +0100

    Use Clique in rootchain server and allow for premine accounts

--- command/rootchain/server/params.go
+++ command/rootchain/server/params.go
@@ -3,9 +3,11 @@ package server
 const (
 	dataDirFlag = "data-dir"
 	noConsole   = "no-console"
+	premineFlag = "premine"
 )
 
 type serverParams struct {
 	dataDir   string
+	premine   []string
 	noConsole bool
 }
