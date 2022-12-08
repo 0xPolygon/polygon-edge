7a0ee2d86cf40b19e5c4eef20131a451031e0602
Ferran Borreguero <ferranbt@protonmail.com>
2022-12-08 11:19:18 +0100

    Add hook interface

---
+++ consensus/polybft/interface.go
@@ -0,0 +1,34 @@
+package polybft
+
+import (
+	"github.com/0xPolygon/polygon-edge/types"
+)
+
+// TaskHook is a lifecycle hook into the life cycle of a block.
+type TaskHook interface {
+	Name() string
+}
+
+type PostBlockRequest struct {
+	Header *types.Header
+}
+
+type TaskPostBlockHook interface {
+	TaskHook
+
+	// PostBlock is called after a block is included
+	// in the chain
+	PostBlock(*PostBlockRequest) error
+}
+
+type PostEpochRequest struct {
+	Header *types.Header
+}
+
+type TaskPostEpochHook interface {
+	TaskHook
+
+	// PostEpoch is called after the end of epoch block
+	// is included in the chain
+	PostEpoch(*PostEpochRequest) error
+}
