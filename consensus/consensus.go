c00b45ac1f9fb714a6a144d790e140505bea0777
Victor Castell <victor@victorcastell.com>
2022-12-14 17:39:40 +0100

    Remove leftover

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -3,7 +3,6 @@ package consensus
 import (
 	"context"
 	"log"
-	"net"
 
 	"github.com/0xPolygon/polygon-edge/blockchain"
 	"github.com/0xPolygon/polygon-edge/chain"
@@ -71,7 +70,6 @@ type Params struct {
 	Blockchain     *blockchain.Blockchain
 	Executor       *state.Executor
 	Grpc           *grpc.Server
-	JSONRPCAddr    *net.TCPAddr
 	Logger         hclog.Logger
 	SecretsManager secrets.SecretsManager
 	BlockTime      uint64
