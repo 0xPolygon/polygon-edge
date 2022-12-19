5f5bcf8ea38623be693fc82ed0dbeeef82b2c786
Stefan Negovanović <93934272+Stefan-Ethernal@users.noreply.github.com>
2022-12-19 14:03:48 +0100

    Consolidate `ExtendByteSlice` implementations (#1058)
    * Consolidate ExtendByteSlice implementations
    
    * Add comment and fix
    
    * Introduce trim parameter (fix unit tests)
    
    * Update bitmap unit tests
    
    * Address comment
    
    * Add a unit test

--- helper/common/common.go
+++ helper/common/common.go
@@ -174,3 +174,14 @@ func PadLeftOrTrim(bb []byte, size int) []byte {
 
 	return tmp
 }
+
+// ExtendByteSlice extends given byte slice by needLength parameter and trims it
+func ExtendByteSlice(b []byte, needLength int) []byte {
+	b = b[:cap(b)]
+
+	if n := needLength - len(b); n > 0 {
+		b = append(b, make([]byte, n)...)
+	}
+
+	return b[:needLength]
+}
