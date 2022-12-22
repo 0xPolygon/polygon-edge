e6e116de4bc1f663cc85c97e1613914b682f30fc
ZeljkoBenovic <sakal87@gmail.com>
2022-12-22 11:18:59 +0100

    added customisation options

--- docker/local/polygon-edge.sh
+++ docker/local/polygon-edge.sh
@@ -3,6 +3,15 @@
 set -e
 
 POLYGON_EDGE_BIN=./polygon-edge
+CHAIN_CUSTOM_OPTIONS=$(tr "\n" " " << EOL
+--block-gas-limit 10000000
+--epoch-size 10
+--chain-id 51001
+--name polygon-edge-docker
+--premine 0x228466F2C715CbEC05dEAbfAc040ce3619d7CF0B:0xD3C21BCECCEDA1000000
+--premine 0xca48694ebcB2548dF5030372BE4dAad694ef174e:0xD3C21BCECCEDA1000000
+EOL
+)
 
 case "$1" in
 
@@ -12,14 +21,14 @@ case "$1" in
               echo "Generating secrets..."
               secrets=$("$POLYGON_EDGE_BIN" secrets init --num 4 --data-dir /data/data- --json)
               echo "Secrets have been successfully generated"
-
               echo "Generating IBFT Genesis file..."
-              cd /data && /polygon-edge/polygon-edge genesis \
+              cd /data && /polygon-edge/polygon-edge genesis  $CHAIN_CUSTOM_OPTIONS \
                 --dir genesis.json \
                 --consensus ibft \
                 --ibft-validators-prefix-path data- \
+                --validator-set-size=4 \
                 --bootnode /dns4/node-1/tcp/1478/p2p/$(echo $secrets | jq -r '.[0] | .node_id') \
-                --bootnode /dns4/node-2/tcp/1478/p2p/$(echo $secrets | jq -r '.[1] | .node_id')
+                --bootnode /dns4/node-2/tcp/1478/p2p/$(echo $secrets | jq -r '.[1] | .node_id') \
               ;;
           "polybft")
               echo "Generating PolyBFT secrets..."
@@ -30,13 +39,11 @@ case "$1" in
               "$POLYGON_EDGE_BIN" manifest --path /data/manifest.json --validators-path /data --validators-prefix data-
 
               echo "Generating PolyBFT Genesis file..."
-              "$POLYGON_EDGE_BIN" genesis \
+              "$POLYGON_EDGE_BIN" genesis $CHAIN_CUSTOM_OPTIONS \
                 --dir /data/genesis.json \
                 --consensus polybft \
                 --manifest /data/manifest.json \
                 --validator-set-size=4 \
-                --block-gas-limit 10000000 \
-                --epoch-size 10 \
                 --bootnode /dns4/node-1/tcp/1478/p2p/$(echo $secrets | jq -r '.[0] | .node_id') \
                 --bootnode /dns4/node-2/tcp/1478/p2p/$(echo $secrets | jq -r '.[1] | .node_id')
               ;;
