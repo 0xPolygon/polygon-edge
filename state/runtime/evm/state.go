25f36e5bedbc8fc2ac0a6bc44593de33efcaa2a2
kourin <kourin.code@gmail.com>
2023-01-05 22:37:08 +0900

    Fix gasCost field in return of debug trace (#1087)
    * Add new error for missing config object in debug trace
    
    * Fix gasCost field in result of debug trace
    
    * Add unit test to make sure new tracer returns error if config is not given
    
    * Fix unit test TestRunWithTracer according to the fixed implementation
    
    * Fix wrong gas consumed for sucessful execution in debug tracing

--- state/runtime/evm/state.go
+++ state/runtime/evm/state.go
@@ -224,7 +224,7 @@ func (c *state) Run() ([]byte, error) {
 
 	for !c.stop {
 		op, ok = c.CurrentOpCode()
-		gasCopy := c.gas
+		gasCopy, ipCopy := c.gas, uint64(c.ip)
 
 		c.captureState(int(op))
 
@@ -237,7 +237,7 @@ func (c *state) Run() ([]byte, error) {
 		inst := dispatchTable[op]
 		if inst.inst == nil {
 			c.exit(errOpCodeNotFound)
-			c.captureExecutionError(op.String(), c.ip, gasCopy)
+			c.captureExecutionError(op.String(), c.ip, gasCopy, 0)
 
 			break
 		}
@@ -245,7 +245,7 @@ func (c *state) Run() ([]byte, error) {
 		// check if the depth of the stack is enough for the instruction
 		if c.sp < inst.stack {
 			c.exit(errStackUnderflow)
-			c.captureExecutionError(op.String(), c.ip, gasCopy)
+			c.captureExecutionError(op.String(), c.ip, gasCopy, inst.gas)
 
 			break
 		}
@@ -253,15 +253,15 @@ func (c *state) Run() ([]byte, error) {
 		// consume the gas of the instruction
 		if !c.consumeGas(inst.gas) {
 			c.exit(errOutOfGas)
-			c.captureExecutionError(op.String(), c.ip, gasCopy)
+			c.captureExecutionError(op.String(), c.ip, gasCopy, inst.gas)
 
 			break
 		}
 
-		c.captureSuccessfulExecution(op.String(), gasCopy)
-
 		// execute the instruction
 		inst.inst(c)
+
+		c.captureSuccessfulExecution(op.String(), ipCopy, gasCopy, gasCopy-c.gas)
 
 		// check if stack size exceeds the max size
 		if c.sp > stackSize {
@@ -402,7 +402,9 @@ func (c *state) captureState(opCode int) {
 
 func (c *state) captureSuccessfulExecution(
 	opCode string,
+	ip uint64,
 	gas uint64,
+	consumedGas uint64,
 ) {
 	tracer := c.host.GetTracer()
 
@@ -412,10 +414,10 @@ func (c *state) captureSuccessfulExecution(
 
 	tracer.ExecuteState(
 		c.msg.Address,
-		uint64(c.ip),
+		ip,
 		opCode,
 		gas,
-		c.currentConsumedGas,
+		consumedGas,
 		c.returnData,
 		c.msg.Depth,
 		c.err,
@@ -427,6 +429,7 @@ func (c *state) captureExecutionError(
 	opCode string,
 	ip int,
 	gas uint64,
+	consumedGas uint64,
 ) {
 	tracer := c.host.GetTracer()
 
@@ -439,7 +442,7 @@ func (c *state) captureExecutionError(
 		uint64(ip),
 		opCode,
 		gas,
-		c.currentConsumedGas,
+		consumedGas,
 		c.returnData,
 		c.msg.Depth,
 		c.err,
