749b1550f3d44ecf1937b0c794ee3f0a4d9189db
Christopher Campbell <ccampbell@polygon.technology>
2023-01-26 14:22:34 -0500

    unfinished invoker endpoint

--- jsonrpc/invoker_endpoint.go
+++ jsonrpc/invoker_endpoint.go
@@ -1,33 +1,12 @@
 package jsonrpc
 
 import (
-	"github.com/0xPolygon/polygon-edge/helper/invoker"
+	"errors"
 )
 
 type Invoker struct {
-	store txPoolStore
 }
 
 func (i *Invoker) SendInvokerTransaction(bufTrans, bufSig argBytes) (interface{}, error) {
-
-	tx := &invoker.InvokerTransaction{}
-	if err := tx.UnmarshalRLP(bufTrans); err != nil {
-		return nil, err
-	}
-
-	// tx.InvokerHash(), tx.ComputeHash() ???
-
-	sig := &invoker.InvokerSignature{}
-	if err := sig.UnmarshalRLP(bufSig); err != nil {
-		return nil, err
-	}
-
-	// TODO ???
-	//if err := i.store.AddTx(tx, sig); err != nil {
-	//	return nil, err
-	//}
-
-	//return tx.Hash.String(), nil
-
-	return nil, nil
+	return nil, errors.New("not implemented")
 }
