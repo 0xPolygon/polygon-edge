d5200f4bcfa8f7413b2d1cf3cfa5f5fc39c47295
Amirul Ashraf <asdacap@gmail.com>
2023-01-30 22:59:27 +0800

    Sync integration on dummy

--- consensus/dummy/dummy.go
+++ consensus/dummy/dummy.go
@@ -1,13 +1,16 @@
 package dummy
 
 import (
+	"fmt"
 	"github.com/0xPolygon/polygon-edge/blockchain"
 	"github.com/0xPolygon/polygon-edge/consensus"
 	"github.com/0xPolygon/polygon-edge/helper/progress"
 	"github.com/0xPolygon/polygon-edge/state"
+	"github.com/0xPolygon/polygon-edge/syncer"
 	"github.com/0xPolygon/polygon-edge/txpool"
 	"github.com/0xPolygon/polygon-edge/types"
-	"github.com/hashicorp/go-hclog"
+	hclog "github.com/hashicorp/go-hclog"
+	"time"
 )
 
 type Dummy struct {
@@ -17,6 +20,7 @@ type Dummy struct {
 	txpool     *txpool.TxPool
 	blockchain *blockchain.Blockchain
 	executor   *state.Executor
+	syncer     syncer.Syncer
 }
 
 func Factory(params *consensus.Params) (consensus.Consensus, error) {
@@ -29,6 +33,13 @@ func Factory(params *consensus.Params) (consensus.Consensus, error) {
 		blockchain: params.Blockchain,
 		executor:   params.Executor,
 		txpool:     params.TxPool,
+
+		syncer: syncer.NewSyncer(
+			params.Logger,
+			params.Network,
+			params.Blockchain,
+			time.Duration(params.BlockTime)*3*time.Second,
+		),
 	}
 
 	return d, nil
@@ -42,6 +53,15 @@ func (d *Dummy) Initialize() error {
 }
 
 func (d *Dummy) Start() error {
+
+	// Start the syncer
+	if err := d.syncer.Start(); err != nil {
+		return err
+	}
+
+	// Start syncing blocks from other peers
+	go d.startSyncing()
+
 	go d.run()
 
 	return nil
@@ -49,6 +69,7 @@ func (d *Dummy) Start() error {
 
 func (d *Dummy) VerifyHeader(header *types.Header) error {
 	// All blocks are valid
+	fmt.Printf("Verifying %d -> %s", header.Number, header.Hash)
 	return nil
 }
 
@@ -79,4 +100,29 @@ func (d *Dummy) run() {
 	d.logger.Info("started")
 	// do nothing
 	<-d.closeCh
+}
+
+func (d *Dummy) startSyncing() {
+	callInsertBlockHook := func(block *types.Block) bool {
+		/*
+			if err := d.currentHooks.PostInsertBlock(block); err != nil {
+				d.logger.Error("failed to call PostInsertBlock", "height", block.Header.Number, "error", err)
+			}
+
+			if err := d.updateCurrentModules(block.Number() + 1); err != nil {
+				d.logger.Error("failed to update sub modules", "height", block.Number()+1, "err", err)
+			}
+		*/
+		d.logger.Info("Block %d", block.Number())
+
+		d.txpool.ResetWithHeaders(block.Header)
+
+		return false
+	}
+
+	if err := d.syncer.Sync(
+		callInsertBlockHook,
+	); err != nil {
+		d.logger.Error("watch sync failed", "err", err)
+	}
 }
