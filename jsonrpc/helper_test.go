51115d9217e791c67eaa0f297d0f5608f3fc7a15
Roman Behma <begmaroman@gmail.com>
2023-02-09 15:58:09 +0000

    Fixed tests

--- jsonrpc/helper_test.go
+++ jsonrpc/helper_test.go
@@ -501,7 +501,7 @@ func TestGetNextNonce(t *testing.T) {
 		name     string
 		address  types.Address
 		num      BlockNumber
-		store    nonceGetter
+		store    dataGetter
 		expected uint64
 		err      bool
 	}{
@@ -642,7 +642,7 @@ func TestDecodeTxn(t *testing.T) {
 	tests := []struct {
 		name     string
 		arg      *txnArgs
-		store    nonceGetter
+		store    dataGetter
 		expected *types.Transaction
 		err      bool
 	}{
