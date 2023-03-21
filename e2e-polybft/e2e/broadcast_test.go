859b7aa13b05317ddd17bdaf149d10b72a7f7d3f
begmaroman <begmaroman@gmail.com>
2023-03-21 10:34:51 +0000

    Minor update

--- e2e-polybft/e2e/broadcast_test.go
+++ e2e-polybft/e2e/broadcast_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/0xPolygon/polygon-edge/types"
 )
 
-// TestE2E_Broadcast sends two transactions (legacy and dynamic fees) to the cluster with the 1 amount of eth
+// TestE2E_Broadcast sends several transactions (legacy and dynamic fees) to the cluster with the 1 amount of eth
 // and checks that all cluster nodes have the recipient balance updated.
 func TestE2E_Broadcast(t *testing.T) {
 	t.Parallel()
