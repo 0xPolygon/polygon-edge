5b678594cd9a088009eafff558df7311b672aed7
begmaroman <begmaroman@gmail.com>
2023-03-21 15:34:34 +0000

    Addressing comments

--- e2e-polybft/e2e/txpool_test.go
+++ e2e-polybft/e2e/txpool_test.go
@@ -6,15 +6,16 @@ import (
 	"testing"
 	"time"
 
-	"github.com/0xPolygon/polygon-edge/consensus/polybft/contractsapi"
-	"github.com/0xPolygon/polygon-edge/e2e-polybft/framework"
-	"github.com/0xPolygon/polygon-edge/txrelayer"
-	"github.com/0xPolygon/polygon-edge/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/umbracle/ethgo"
 	"github.com/umbracle/ethgo/jsonrpc"
 	"github.com/umbracle/ethgo/wallet"
+
+	"github.com/0xPolygon/polygon-edge/consensus/polybft/contractsapi"
+	"github.com/0xPolygon/polygon-edge/e2e-polybft/framework"
+	"github.com/0xPolygon/polygon-edge/txrelayer"
+	"github.com/0xPolygon/polygon-edge/types"
 )
 
 func TestE2E_TxPool_Transfer(t *testing.T) {
@@ -58,12 +59,14 @@ func TestE2E_TxPool_Transfer(t *testing.T) {
 
 			// Send every second transaction as a dynamic fees one
 			if i%2 == 0 {
+				txn.Type = ethgo.TransactionDynamicFee
 				txn.MaxFeePerGas = big.NewInt(1000000000)
 				txn.MaxPriorityFeePerGas = big.NewInt(100000000)
 			} else {
 				gasPrice, err := client.GasPrice()
 				require.NoError(t, err)
 
+				txn.Type = ethgo.TransactionLegacy
 				txn.GasPrice = gasPrice
 			}
 
@@ -158,9 +161,11 @@ func TestE2E_TxPool_Transfer_Linear(t *testing.T) {
 
 		// Send every second transaction as a dynamic fees one
 		if i%2 == 0 {
+			txn.Type = ethgo.TransactionDynamicFee
 			txn.MaxFeePerGas = big.NewInt(1000000000)
 			txn.MaxPriorityFeePerGas = big.NewInt(100000000)
 		} else {
+			txn.Type = ethgo.TransactionLegacy
 			txn.GasPrice = gasPrice
 		}
 
@@ -202,12 +207,95 @@ func TestE2E_TxPool_TransactionWithHeaderInstuctions(t *testing.T) {
 	require.NoError(t, cluster.WaitForBlock(10, 1*time.Minute))
 }
 
+// TestE2E_TxPool_Broadcast sends several transactions (legacy and dynamic fees) to the cluster with the 1 amount of eth
+// and checks that all cluster nodes have the recipient balance updated.
+func TestE2E_TxPool_Broadcast(t *testing.T) {
+	var (
+		sendAmount = ethgo.Ether(1)
+	)
+
+	const (
+		txNum = 10
+	)
+
+	// Create recipient key
+	key, err := wallet.GenerateKey()
+	assert.NoError(t, err)
+
+	recipient := key.Address()
+
+	t.Logf("Recipient %s\n", recipient)
+
+	// Create pre-mined balance for sender
+	sender, err := wallet.GenerateKey()
+	require.NoError(t, err)
+
+	// First account should have some matics premined
+	cluster := framework.NewTestCluster(t, 5,
+		framework.WithPremine(types.Address(sender.Address())),
+	)
+	defer cluster.Stop()
+
+	// Wait until the cluster is up and running
+	cluster.WaitForReady(t)
+
+	client := cluster.Servers[0].JSONRPC().Eth()
+
+	sentAmount := new(big.Int)
+	nonce := uint64(0)
+
+	for i := 0; i < txNum; i++ {
+		txn := &ethgo.Transaction{
+			Value: sendAmount,
+			To:    &recipient,
+			Gas:   21000,
+			Nonce: nonce,
+		}
+
+		if i%2 == 0 {
+			txn.Type = ethgo.TransactionDynamicFee
+			txn.MaxFeePerGas = big.NewInt(1000000000)
+			txn.MaxPriorityFeePerGas = big.NewInt(100000000)
+		} else {
+			gasPrice, err := client.GasPrice()
+			require.NoError(t, err)
+
+			txn.Type = ethgo.TransactionLegacy
+			txn.GasPrice = gasPrice
+		}
+
+		sendTransaction(t, client, sender, txn)
+		sentAmount = sentAmount.Add(sentAmount, txn.Value)
+		nonce++
+	}
+
+	// Wait until the balance has changed on all nodes in the cluster
+	err = cluster.WaitUntil(time.Minute, time.Second*3, func() bool {
+		for _, srv := range cluster.Servers {
+			balance, err := srv.WaitForNonZeroBalance(recipient, time.Second*10)
+			assert.NoError(t, err)
+			if balance != nil && balance.BitLen() > 0 {
+				assert.Equal(t, sentAmount, balance)
+			} else {
+				return false
+			}
+		}
+
+		return true
+	})
+	assert.NoError(t, err)
+}
+
 // sendTransaction is a helper function which signs transaction with provided private key and sends it
 func sendTransaction(t *testing.T, client *jsonrpc.Eth, sender *wallet.Key, txn *ethgo.Transaction) {
 	t.Helper()
 
 	chainID, err := client.ChainID()
 	require.NoError(t, err)
+
+	if txn.Type == ethgo.TransactionDynamicFee {
+		txn.ChainID = chainID
+	}
 
 	signer := wallet.NewEIP155Signer(chainID.Uint64())
 	signedTxn, err := signer.SignTx(txn, sender)
