orgTestCodacy11KRepos110MB/repo-5686-polygon-edge

Add stake flag to manifest command

command/rootchain/initcontracts/init_contracts.go
@@ -354,7 +354,7 @@ func validatorSetToABISlice(validators []*polybft.Validator) ([]map[string]inter
 		accSet[i] = &polybft.ValidatorMetadata{
 			Address:     validatorInfo.Address,
 			BlsKey:      blsKey,
-			VotingPower: new(big.Int).Set(validatorInfo.Balance),
+			VotingPower: new(big.Int).Set(validatorInfo.Stake),
 		}
 	}
 

0xPolygon/polygon-edge

command/rootchain/initcontracts/init_contracts.go
@@ -463,7 +463,7 @@ func validatorSetToABISlice(o command.OutputFormatter,
 		accSet[i] = &polybft.ValidatorMetadata{
 			Address:     validator.Address,
 			BlsKey:      blsKey,
-			VotingPower: new(big.Int).Set(validator.Balance),
+			VotingPower: new(big.Int).Set(validator.Stake),
 		}
 	}
 