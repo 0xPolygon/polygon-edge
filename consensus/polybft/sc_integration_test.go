940c2cf8887630d08f43f80e7c809b23024c5d44
Igor Crevar <crewce@gmail.com>
2023-04-05 15:18:10 +0200

    deposit + widthdraw signature

--- consensus/polybft/sc_integration_test.go
+++ consensus/polybft/sc_integration_test.go
@@ -14,6 +14,7 @@ import (
 	bls "github.com/0xPolygon/polygon-edge/consensus/polybft/signer"
 	"github.com/0xPolygon/polygon-edge/consensus/polybft/wallet"
 	"github.com/0xPolygon/polygon-edge/contracts"
+	"github.com/0xPolygon/polygon-edge/crypto"
 	secretsHelper "github.com/0xPolygon/polygon-edge/secrets/helper"
 	"github.com/0xPolygon/polygon-edge/state"
 	"github.com/0xPolygon/polygon-edge/types"
@@ -28,21 +29,25 @@ func TestIntegratoin_PerformExit(t *testing.T) {
 
 	const gasPrice = 1000000000000
 
-	// create validator set
+	// create validator set and checkpoint mngr
 	currentValidators := newTestValidatorsWithAliases(t, []string{"A", "B", "C", "D"}, []uint64{100, 100, 100, 100})
 	accSet := currentValidators.getPublicIdentities()
+	cm := checkpointManager{blockchain: &blockchainMock{}}
 
 	senderAddress := types.Address{1}
 	bn256Addr := types.Address{2}
 	rootERC20Token := types.Address{4}
 	stateSenderAddr := types.Address{5}
 	receiverAddr := types.Address{6}
+	amount1 := big.NewInt(3)
+	amount2 := big.NewInt(2)
 
 	alloc := map[types.Address]*chain.GenesisAccount{
 		senderAddress:         {Balance: big.NewInt(100000000000)},
 		contracts.BLSContract: {Code: contractsapi.BLS.DeployedBytecode},
 		bn256Addr:             {Code: contractsapi.BLS256.DeployedBytecode},
 		rootERC20Token:        {Code: contractsapi.RootERC20.DeployedBytecode},
+		stateSenderAddr:       {Code: contractsapi.StateSender.DeployedBytecode},
 	}
 	transition := newTestTransition(t, alloc)
 
@@ -60,45 +65,36 @@ func TestIntegratoin_PerformExit(t *testing.T) {
 
 	// deploy CheckpointManager
 	checkpointManagerInit := func() ([]byte, error) {
-		initialize := contractsapi.InitializeCheckpointManagerFn{
+		return (&contractsapi.InitializeCheckpointManagerFn{
 			NewBls:          contracts.BLSContract,
 			NewBn256G2:      bn256Addr,
 			NewValidatorSet: accSet.ToAPIBinding(),
 			ChainID_:        big.NewInt(0),
-		}
-
-		return initialize.EncodeAbi()
+		}).EncodeAbi()
 	}
 
 	checkpointManagerAddr := deployAndInitContract(t, transition, contractsapi.CheckpointManager, senderAddress, checkpointManagerInit)
 
 	// deploy ExitHelper
 	exitHelperInit := func() ([]byte, error) {
-		initialize := &contractsapi.InitializeExitHelperFn{NewCheckpointManager: checkpointManagerAddr}
-
-		return initialize.EncodeAbi()
+		return (&contractsapi.InitializeExitHelperFn{NewCheckpointManager: checkpointManagerAddr}).EncodeAbi()
 	}
 	exitHelperContractAddress := deployAndInitContract(t, transition, contractsapi.ExitHelper, senderAddress, exitHelperInit)
 
 	// deploy RootERC20Predicate
 	rootERC20PredicateInit := func() ([]byte, error) {
-		initialize := &contractsapi.InitializeRootERC20PredicateFn{
+		return (&contractsapi.InitializeRootERC20PredicateFn{
 			NewStateSender:         stateSenderAddr,
 			NewExitHelper:          exitHelperContractAddress,
 			NewChildERC20Predicate: contracts.ChildERC20PredicateContract,
 			NewChildTokenTemplate:  contracts.ChildERC20Contract,
 			NativeTokenRootAddress: contracts.NativeERC20TokenContract,
-		}
-
-		return initialize.EncodeAbi()
+		}).EncodeAbi()
 	}
 	rootERC20PredicateAddr := deployAndInitContract(t, transition, contractsapi.RootERC20Predicate, senderAddress, rootERC20PredicateInit)
 
 	require.Equal(t, getField(checkpointManagerAddr, contractsapi.CheckpointManager.Abi, "currentCheckpointBlockNumber")[31], uint8(0))
 
-	cm := checkpointManager{
-		blockchain: &blockchainMock{},
-	}
 	accSetHash, err := accSet.Hash()
 	require.NoError(t, err)
 
@@ -107,21 +103,36 @@ func TestIntegratoin_PerformExit(t *testing.T) {
 	epochNumber := uint64(1)
 	blockRound := uint64(1)
 
-	erc20DataType := abi.MustNewType("tuple(address rootToken, address withdrawer, address receiver, uint256 amount)")
+	// deposit
+	depositFn, err := (&contractsapi.DepositToRootERC20PredicateFn{
+		RootToken: rootERC20Token,
+		Receiver:  receiverAddr,
+		Amount:    new(big.Int).Add(amount1, amount2),
+	}).EncodeAbi()
+	require.NoError(t, err)
+
+	result := transition.Call2(senderAddress, rootERC20PredicateAddr, depositFn, big.NewInt(0), 1000000000)
+	require.NoError(t, result.Err)
+
+	widthdrawSig := crypto.Keccak256([]byte("WITHDRAW"))
+	erc20DataType := abi.MustNewType(
+		"tuple(bytes32 withdrawSignature, address rootToken, address withdrawer, address receiver, uint256 amount)")
 
 	exitData1, err := erc20DataType.Encode(map[string]interface{}{
-		"rootToken":  ethgo.Address(rootERC20Token),
-		"withdrawer": ethgo.Address(senderAddress),
-		"receiver":   ethgo.Address(receiverAddr),
-		"amount":     big.NewInt(3),
+		"withdrawSignature": widthdrawSig,
+		"rootToken":         ethgo.Address(rootERC20Token),
+		"withdrawer":        ethgo.Address(senderAddress),
+		"receiver":          ethgo.Address(receiverAddr),
+		"amount":            amount1,
 	})
 	require.NoError(t, err)
 
 	exitData2, err := erc20DataType.Encode(map[string]interface{}{
-		"rootToken":  ethgo.Address(rootERC20Token),
-		"withdrawer": ethgo.Address(senderAddress),
-		"receiver":   ethgo.Address(receiverAddr),
-		"amount":     big.NewInt(2),
+		"withdrawSignature": widthdrawSig,
+		"rootToken":         ethgo.Address(rootERC20Token),
+		"withdrawer":        ethgo.Address(senderAddress),
+		"receiver":          ethgo.Address(receiverAddr),
+		"amount":            amount2,
 	})
 	require.NoError(t, err)
 
@@ -158,10 +169,9 @@ func TestIntegratoin_PerformExit(t *testing.T) {
 		blockHash)
 	require.NoError(t, err)
 
-	bmp := bitmap.Bitmap{}
 	i := uint64(0)
-
-	var signatures bls.Signatures
+	bmp := bitmap.Bitmap{}
+	signatures := bls.Signatures(nil)
 
 	currentValidators.iterAcct(nil, func(v *testValidator) {
 		signatures = append(signatures, v.mustSign(checkpointHash[:], bls.DomainCheckpointManager))
@@ -174,17 +184,17 @@ func TestIntegratoin_PerformExit(t *testing.T) {
 
 	extra := &Extra{
 		Checkpoint: &checkpointData,
-	}
-	extra.Committed = &Signature{
-		AggregatedSignature: aggSignature,
-		Bitmap:              bmp,
+		Committed: &Signature{
+			AggregatedSignature: aggSignature,
+			Bitmap:              bmp,
+		},
 	}
 
 	// submit a checkpoint
 	submitCheckpointEncoded, err := cm.abiEncodeCheckpointBlock(blockNumber, blockHash, extra, accSet)
 	require.NoError(t, err)
 
-	result := transition.Call2(senderAddress, checkpointManagerAddr, submitCheckpointEncoded, big.NewInt(0), 1000000000)
+	result = transition.Call2(senderAddress, checkpointManagerAddr, submitCheckpointEncoded, big.NewInt(0), 1000000000)
 	require.NoError(t, result.Err)
 	require.Equal(t, getField(checkpointManagerAddr, contractsapi.CheckpointManager.Abi, "currentCheckpointBlockNumber")[31], uint8(1))
 
@@ -202,14 +212,12 @@ func TestIntegratoin_PerformExit(t *testing.T) {
 	leafIndex, err := exitTree.LeafIndex(proofExitEvent)
 	require.NoError(t, err)
 
-	exitFn := &contractsapi.ExitExitHelperFn{
+	exitFnInput, err := (&contractsapi.ExitExitHelperFn{
 		BlockNumber:  new(big.Int).SetUint64(blockNumber),
 		LeafIndex:    new(big.Int).SetUint64(leafIndex),
 		UnhashedLeaf: proofExitEvent,
 		Proof:        proof,
-	}
-
-	exitFnInput, err := exitFn.EncodeAbi()
+	}).EncodeAbi()
 	require.NoError(t, err)
 
 	result = transition.Call2(senderAddress, exitHelperContractAddress, exitFnInput, big.NewInt(0), 1000000000)
@@ -217,16 +225,16 @@ func TestIntegratoin_PerformExit(t *testing.T) {
 
 	// check true
 	res = getField(exitHelperContractAddress, contractsapi.ExitHelper.Abi, "processedExits", exits[0].ID)
-	require.Equal(t, 1, int(res[31]))
+	require.Equal(t, types.ReceiptSuccess, int(res[31]))
 
-	// lastID := getField(l1StateReceiverAddr, contractsapi.TestL1StateReceiver.Abi, "id")
-	// require.Equal(t, uint8(1), lastID[31])
+	lastID := getField(rootERC20Token, contractsapi.RootERC20.Abi, "id")
+	require.Equal(t, uint8(1), lastID[31])
 
-	// lastAddr := getField(l1StateReceiverAddr, contractsapi.TestL1StateReceiver.Abi, "addr")
-	// require.Equal(t, exits[0].Sender[:], lastAddr[12:])
+	lastAddr := getField(rootERC20Token, contractsapi.RootERC20.Abi, "addr")
+	require.Equal(t, exits[0].Sender[:], lastAddr[12:])
 
-	// lastCounter := getField(l1StateReceiverAddr, contractsapi.TestL1StateReceiver.Abi, "counter")
-	// require.Equal(t, lastCounter[31], uint8(1))
+	lastCounter := getField(rootERC20Token, contractsapi.RootERC20.Abi, "counter")
+	require.Equal(t, uint8(1), lastCounter[31])
 }
 
 func TestIntegration_CommitEpoch(t *testing.T) {
@@ -368,17 +376,16 @@ func deployAndInitContract(t *testing.T, transition *state.Transition, scArtifac
 	initCallback func() ([]byte, error)) types.Address {
 	t.Helper()
 
-	result := transition.Create2(sender, scArtifact.Bytecode, big.NewInt(0), 1e9)
-	assert.NoError(t, result.Err)
-	addr := result.Address
+	deployResult := transition.Create2(sender, scArtifact.Bytecode, big.NewInt(0), 1e9)
+	assert.NoError(t, deployResult.Err)
 
 	if initCallback != nil {
 		initInput, err := initCallback()
 		require.NoError(t, err)
 
-		result = transition.Call2(sender, addr, initInput, big.NewInt(0), 1e9)
+		result := transition.Call2(sender, deployResult.Address, initInput, big.NewInt(0), 1e9)
 		require.NoError(t, result.Err)
 	}
 
-	return addr
+	return deployResult.Address
 }
