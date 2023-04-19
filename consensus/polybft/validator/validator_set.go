366d33afc8fc5feb9766c96d63b085413845b9ba
Nemanja0x <nemanja@ethernal.tech>
2023-04-19 10:21:32 +0200

    introduce new version of go ibft

--- consensus/polybft/validator/validator_set.go
+++ consensus/polybft/validator/validator_set.go
@@ -21,6 +21,9 @@ type ValidatorSet interface {
 
 	// checks if submitted signers have reached quorum
 	HasQuorum(signers map[types.Address]struct{}) bool
+
+	// retrieves map: string(address) -> vp
+	GetVotingPowers() map[string]*big.Int
 }
 
 type validatorSet struct {
@@ -78,6 +81,16 @@ func (vs validatorSet) HasQuorum(signers map[types.Address]struct{}) bool {
 		"hasQuorum", hasQuorum)
 
 	return hasQuorum
+}
+
+func (vs validatorSet) GetVotingPowers() map[string]*big.Int {
+	result := make(map[string]*big.Int, vs.Len())
+
+	for address, vp := range vs.votingPowerMap {
+		result[types.AddressToString(address)] = vp
+	}
+
+	return result
 }
 
 func (vs validatorSet) Accounts() AccountSet {
