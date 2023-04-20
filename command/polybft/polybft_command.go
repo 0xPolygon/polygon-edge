9ff1cb1f0e4454a1e7d89aa2f4b63af66e296566
Goran Rojovic <goran.rojovic@ethernal.tech>
2023-04-20 12:37:12 +0200

    Build fix

--- command/polybft/polybft_command.go
+++ command/polybft/polybft_command.go
@@ -1,12 +1,10 @@
 package polybft
 
 import (
-	"github.com/0xPolygon/polygon-edge/command/sidechain/registration"
 	"github.com/0xPolygon/polygon-edge/command/sidechain/staking"
 	"github.com/0xPolygon/polygon-edge/command/sidechain/unstaking"
 	"github.com/0xPolygon/polygon-edge/command/sidechain/validators"
 
-	"github.com/0xPolygon/polygon-edge/command/sidechain/whitelist"
 	"github.com/0xPolygon/polygon-edge/command/sidechain/withdraw"
 	"github.com/spf13/cobra"
 )
@@ -22,8 +20,6 @@ func GetCommand() *cobra.Command {
 		unstaking.GetCommand(),
 		withdraw.GetCommand(),
 		validators.GetCommand(),
-		whitelist.GetCommand(),
-		registration.GetCommand(),
 	)
 
 	return polybftCmd
