0723f8cb8e9b7bcfafa08bf3759971bd599afe9a
Victor Castell <victor@polygon.technology>
2023-05-04 14:28:12 +0200

    Lint

--- consensus/polybft/contracts_initializer.go
+++ consensus/polybft/contracts_initializer.go
@@ -84,6 +84,7 @@ func getInitChildERC20PredicateAccessListInput(config PolyBFTConfig) ([]byte, er
 	rootERC20PredicateAddr := types.StringToAddress(disabledBridgeRootPredicateAddr)
 	rootERC20Addr := types.ZeroAddress
 
+	//nolint:godox
 	// TODO: This can be removed as we'll always have a bridge config
 	if config.Bridge != nil {
 		rootERC20PredicateAddr = config.Bridge.RootERC20PredicateAddr
