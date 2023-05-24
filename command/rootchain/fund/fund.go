d6ad73515786334c6df53a235bbc3cc6248378f6
dusan-maksimovic <dusan.maksimovic@ethernal.tech>
2023-05-24 09:40:11 +0200

    Modified rootchain command to use the provided private key to fund validators

--- command/rootchain/fund/fund.go
+++ command/rootchain/fund/fund.go
@@ -96,7 +96,7 @@ func runCommand(cmd *cobra.Command, _ []string) {
 		stakeTokenAddr types.Address
 	)
 
-	if params.mintStakeToken {
+	if params.mintStakeToken || params.deployerPrivateKey != "" {
 		deployerKey, err = helper.GetRootchainPrivateKey(params.deployerPrivateKey)
 		if err != nil {
 			outputter.SetError(fmt.Errorf("failed to initialize deployer private key: %w", err))
@@ -126,7 +126,14 @@ func runCommand(cmd *cobra.Command, _ []string) {
 					Value: params.amountValues[i],
 				}
 
-				receipt, err := txRelayer.SendTransactionLocal(txn)
+				var receipt *ethgo.Receipt
+
+				if params.deployerPrivateKey != "" {
+					receipt, err = txRelayer.SendTransaction(txn, deployerKey)
+				} else {
+					receipt, err = txRelayer.SendTransactionLocal(txn)
+				}
+
 				if err != nil {
 					return fmt.Errorf("failed to send fund validator '%s' transaction: %w", validatorAddr, err)
 				}
