c71fd90f081435f464f3b66b6479e7be650fe3c6
begmaroman <begmaroman@gmail.com>
2023-05-26 14:13:06 +0100

    Implementation

--- jsonrpc/eth_blockchain_test.go
+++ jsonrpc/eth_blockchain_test.go
@@ -95,7 +95,7 @@ func TestEth_Block_GetBlockTransactionCountByNumber(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.NotNil(t, res, "expected to return block, but got nil")
-	assert.Equal(t, res, types.EncodeUint64(10))
+	assert.Equal(t, "0xa", res)
 }
 
 func TestEth_GetTransactionByHash(t *testing.T) {
