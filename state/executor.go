089cb40afd7d796f0b1141d1b24751151ca4f026
Juan Palacios <juan.palacios@immutable.com>
2023-06-26 20:21:55 +1000

    Added DEBUG level log lines when the transaction or deployment allowlists are applied to reject a transaction

--- state/executor.go
+++ state/executor.go
@@ -690,6 +690,11 @@ func (t *Transition) run(contract *runtime.Contract, host runtime.Host) *runtime
 		if contract.Caller != contracts.SystemCaller {
 			role := t.txnAllowList.GetRole(contract.Caller)
 			if !role.Enabled() {
+				t.logger.Debug(
+					"Failing transaction. Caller is not in the transaction allowlist",
+					"contract.Caller", contract.Caller,
+					"contract.Address", contract.Address,
+				)
 				return &runtime.ExecutionResult{
 					GasLeft: 0,
 					Err:     runtime.ErrNotAuth,
@@ -704,6 +709,11 @@ func (t *Transition) run(contract *runtime.Contract, host runtime.Host) *runtime
 		if contract.Caller != contracts.SystemCaller {
 			role := t.txnBlockList.GetRole(contract.Caller)
 			if role == addresslist.EnabledRole {
+				t.logger.Debug(
+					"Failing transaction. Caller is in the transaction blocklist",
+					"contract.Caller", contract.Caller,
+					"contract.Address", contract.Address,
+				)
 				return &runtime.ExecutionResult{
 					GasLeft: 0,
 					Err:     runtime.ErrNotAuth,
@@ -855,6 +865,11 @@ func (t *Transition) applyCreate(c *runtime.Contract, host runtime.Host) *runtim
 		role := t.deploymentAllowList.GetRole(c.Caller)
 
 		if !role.Enabled() {
+			t.logger.Debug(
+				"Failing contract deployment. Caller is not in the deployment allowlist",
+				"contract.Caller", c.Caller,
+				"contract.Address", c.Address,
+			)
 			return &runtime.ExecutionResult{
 				GasLeft: 0,
 				Err:     runtime.ErrNotAuth,
@@ -864,6 +879,11 @@ func (t *Transition) applyCreate(c *runtime.Contract, host runtime.Host) *runtim
 		role := t.deploymentBlockList.GetRole(c.Caller)
 
 		if role == addresslist.EnabledRole {
+			t.logger.Debug(
+				"Failing contract deployment. Caller is in the deployment blocklist",
+				"contract.Caller", c.Caller,
+				"contract.Address", c.Address,
+			)
 			return &runtime.ExecutionResult{
 				GasLeft: 0,
 				Err:     runtime.ErrNotAuth,
