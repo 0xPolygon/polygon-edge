7f25c4ebaf15a20dd9d5e48a172c8977adad8f2c
Stefan Negovanović <stefan@ethernal.tech>
2023-06-26 10:59:39 +0200

    Print additional fields in the withdraw CLI command

--- command/sidechain/withdraw/params.go
+++ command/sidechain/withdraw/params.go
@@ -3,6 +3,7 @@ package withdraw
 import (
 	"bytes"
 	"fmt"
+	"math/big"
 
 	"github.com/0xPolygon/polygon-edge/command/helper"
 	sidechainHelper "github.com/0xPolygon/polygon-edge/command/sidechain"
@@ -14,23 +15,27 @@ type withdrawParams struct {
 	jsonRPC       string
 }
 
+func (w *withdrawParams) validateFlags() error {
+	return sidechainHelper.ValidateSecretFlags(w.accountDir, w.accountConfig)
+}
+
 type withdrawResult struct {
 	validatorAddress string
-	amount           uint64
+	amount           *big.Int
+	exitEventID      *big.Int
+	blockNumber      uint64
 }
 
-func (v *withdrawParams) validateFlags() error {
-	return sidechainHelper.ValidateSecretFlags(v.accountDir, v.accountConfig)
-}
-
-func (ur withdrawResult) GetOutput() string {
+func (r *withdrawResult) GetOutput() string {
 	var buffer bytes.Buffer
 
 	buffer.WriteString("\n[WITHDRAWAL]\n")
 
-	vals := make([]string, 0, 2)
-	vals = append(vals, fmt.Sprintf("Validator Address|%s", ur.validatorAddress))
-	vals = append(vals, fmt.Sprintf("Amount Withdrawn|%v", ur.amount))
+	vals := make([]string, 0, 4)
+	vals = append(vals, fmt.Sprintf("Validator Address|%s", r.validatorAddress))
+	vals = append(vals, fmt.Sprintf("Amount Withdrawn|%d", r.amount))
+	vals = append(vals, fmt.Sprintf("Exit Event ID|%d", r.exitEventID))
+	vals = append(vals, fmt.Sprintf("Inclusion Block Number|%d", r.blockNumber))
 
 	buffer.WriteString(helper.FormatKV(vals))
 	buffer.WriteString("\n")
