599b89409171232b43e8e83db9a483f41f5ab6e7
Rachit Sonthalia <rachitsonthalia02@gmail.com>
2023-06-29 17:32:04 +0530

    fix for EVM-719(without taking a lock for syncPeers)

--- syncer/client.go
+++ syncer/client.go
@@ -161,13 +161,12 @@ func (m *syncPeerClient) GetConnectedPeerStatuses() []*NoForkPeer {
 			status, err := m.GetPeerStatus(peerID)
 			if err != nil {
 				m.logger.Warn("failed to get status from a peer, skip", "id", peerID, "err", err)
+				return //Skip appending nil status
 			}
 
 			syncPeersLock.Lock()
 
-			if status != nil {
-				syncPeers = append(syncPeers, status)
-			}
+			syncPeers = append(syncPeers, status)
 
 			syncPeersLock.Unlock()
 		}()
