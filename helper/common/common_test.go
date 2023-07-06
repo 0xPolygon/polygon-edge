2cb7814b4780d7134e16a9ecd9cade272de03885
sergerad <serge.radinovich@immutable.com>
2023-07-06 15:17:22 +1200

    Stop retrying event tracker on context cancellation and timeout

--- helper/common/common_test.go
+++ helper/common/common_test.go
@@ -116,19 +116,11 @@ func TestRetryForever_AlwaysReturnError_ShouldNeverEnd(t *testing.T) {
 	require.False(t, ended)
 }
 
-func TestRetryForever_ReturnNilAfterFirstRun_ShouldEnd(t *testing.T) {
+func TestRetryForever_CancelContext_ShouldEnd(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	cancel()
 	RetryForever(ctx, time.Millisecond*100, func(ctx context.Context) error {
-		select {
-		case <-ctx.Done():
-
-			return nil
-		default:
-			cancel()
-
-			return errors.New("")
-		}
+		return errors.New("")
 	})
-	<-ctx.Done()
 	require.True(t, errors.Is(ctx.Err(), context.Canceled))
 }
--- tracker/event_tracker.go
+++ tracker/event_tracker.go
@@ -80,17 +80,21 @@ func (e *EventTracker) Start(ctx context.Context) error {
 
 	// Init and start block tracker concurrently, retrying indefinitely
 	go common.RetryForever(ctx, time.Second, func(context.Context) error {
+		// Init
 		start := time.Now().UTC()
-
 		if err := blockTracker.Init(); err != nil {
 			e.logger.Error("failed to init blocktracker", "error", err)
 
 			return err
 		}
+		elapsed := time.Now().UTC().Sub(start)
 
-		elapsed := time.Now().UTC().Sub(start) // Calculate the elapsed time
-
+		// Start
 		if err := blockTracker.Start(); err != nil {
+			if common.IsContextDone(err) {
+				return nil
+			}
+
 			e.logger.Error("failed to start blocktracker", "error", err)
 
 			return err
@@ -117,8 +121,12 @@ func (e *EventTracker) Start(ctx context.Context) error {
 		return err
 	}
 	// Sync concurrently, retrying indefinitely
-	go common.RetryForever(ctx, time.Second, func(context.Context) error {
+	go common.RetryForever(ctx, time.Second, func(ctx context.Context) error {
 		if err := tt.Sync(ctx); err != nil {
+			if common.IsContextDone(err) {
+				return nil
+			}
+
 			e.logger.Error("failed to sync", "error", err)
 
 			return err
