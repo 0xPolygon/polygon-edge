e12ee06d761ee95dddca9348e0bc1dca9e40dc2e
Marko Jelaca <jelaca.marko@gmail.com>
2023-08-30 15:09:02 +0200

    Return all exit event IDs in the bridge commands

--- command/bridge/common/params.go
+++ command/bridge/common/params.go
@@ -143,8 +143,11 @@ func (bp *ERC1155BridgeParams) Validate() error {
 }
 
 // ExtractExitEventID tries to extract exit event id from provided receipt
-func ExtractExitEventID(receipt *ethgo.Receipt) (*big.Int, error) {
+func ExtractExitEventID(receipt *ethgo.Receipt) ([]*big.Int, error) {
 	var exitEvent contractsapi.L2StateSyncedEvent
+
+	exitEventIDs := make([]*big.Int, 0, len(receipt.Logs))
+
 	for _, log := range receipt.Logs {
 		doesMatch, err := exitEvent.ParseLog(log)
 		if err != nil {
@@ -155,7 +158,11 @@ func ExtractExitEventID(receipt *ethgo.Receipt) (*big.Int, error) {
 			continue
 		}
 
-		return exitEvent.ID, nil
+		exitEventIDs = append(exitEventIDs, exitEvent.ID)
+	}
+
+	if len(exitEventIDs) != 0 {
+		return exitEventIDs, nil
 	}
 
 	return nil, errors.New("failed to find exit event log")
