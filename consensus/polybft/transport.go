749a5bfb77c18291c9aa2ab3a4779bd2be7a394c
Dusan <dusan.nosovic@ethernal.tech>
2023-08-31 10:06:24 +0200

    finished code with logs

--- consensus/polybft/transport.go
+++ consensus/polybft/transport.go
@@ -1,12 +1,16 @@
 package polybft
 
 import (
+	"crypto/rand"
 	"fmt"
 
 	ibftProto "github.com/0xPolygon/go-ibft/messages/proto"
 	polybftProto "github.com/0xPolygon/polygon-edge/consensus/polybft/proto"
+	"github.com/0xPolygon/polygon-edge/crypto"
 	"github.com/0xPolygon/polygon-edge/types"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"google.golang.org/protobuf/proto"
+	protobuf "google.golang.org/protobuf/proto"
 )
 
 // BridgeTransport is an abstraction of network layer for a bridge
@@ -59,7 +63,71 @@ func (p *Polybft) createTopics() (err error) {
 
 // Multicast is implementation of core.Transport interface
 func (p *Polybft) Multicast(msg *ibftProto.Message) {
+	p.logger.Error(fmt.Sprintf("[MulticastPublish]"))
+
+	if msg.Type == ibftProto.MessageType_COMMIT {
+		sender := types.BytesToAddress(msg.From)
+		localAddr := types.Address(p.key.Address())
+		p.logger.Debug("[Multicast]", "msg sender", sender.String(), "local node address", localAddr.String())
+
+		if sender == localAddr {
+			tamperedMsg := proto.Clone(msg).(*ibftProto.Message)
+			tamperedMsg.GetCommitData().ProposalHash = generateRandomHash()
+			tamperedMsg.Signature = nil
+
+			tamperedMsgRaw, _ := protobuf.Marshal(tamperedMsg)
+
+			p.logger.Error("MsgRaw", "before", tamperedMsgRaw, "cryptokeccakBefore:", crypto.Keccak256(tamperedMsgRaw))
+
+			tamperedMsg, err := p.key.SignIBFTMessage(tamperedMsg)
+
+			if err != nil {
+				p.logger.Error("Error while signing", "error", err)
+			}
+
+			p.logger.Error(fmt.Sprintf("[Polybft]Multicast(publish1): %+v", tamperedMsg))
+			msgNoSigTampered, err := tamperedMsg.PayloadNoSig()
+
+			if err != nil {
+				p.logger.Error("NoPayload")
+			}
+
+			pub, _ := crypto.RecoverPubkey(tamperedMsg.Signature, crypto.Keccak256(msgNoSigTampered))
+
+			p.logger.Error("msgRawAfter", "after", msgNoSigTampered, "CryptoKeccakAfter", crypto.Keccak256(msgNoSigTampered))
+			p.logger.Error("PubKey", "after", pub)
+			signerAddress, err := RecoverAddressFromSignature(tamperedMsg.Signature, msgNoSigTampered)
+
+			if err != nil {
+				p.logger.Error("failed to recover address from signature: %w", err)
+			}
+
+			p.logger.Error("Signer", "Address", signerAddress.String())
+
+			err = p.consensusTopic.Publish(tamperedMsg)
+			if err != nil {
+				p.logger.Error("Error while sending byzantian message", "error", err)
+			}
+		}
+	}
+
+	p.logger.Error("[Polybft]Multicast(publish2): %+v", msg)
 	if err := p.consensusTopic.Publish(msg); err != nil {
 		p.logger.Warn("failed to multicast consensus message", "error", err)
 	}
+}
+func RecoverAddressFromSignature(sig, rawContent []byte) (types.Address, error) {
+	pub, err := crypto.RecoverPubkey(sig, crypto.Keccak256(rawContent))
+	if err != nil {
+		return types.Address{}, fmt.Errorf("cannot recover address from signature: %w", err)
+	}
+
+	return crypto.PubKeyToAddress(pub), nil
+}
+
+func generateRandomHash() []byte {
+	result := make([]byte, types.HashLength)
+	_, _ = rand.Reader.Read(result)
+
+	return result
 }
