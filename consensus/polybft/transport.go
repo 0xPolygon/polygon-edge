8e5f0469c5bc87926a33151cfc3465a9f0e6cd7a
dusan-maksimovic <dusan.maksimovic@ethernal.tech>
2023-09-14 15:53:34 +0200

    Revert "Expose double signer implementation (#1875)"
    This reverts commit 3d2e58fc8306f994cab58aa9ba8f12e30667432c.

--- consensus/polybft/transport.go
+++ consensus/polybft/transport.go
@@ -71,8 +71,6 @@ func (p *Polybft) createTopics() (err error) {
 
 // Multicast is implementation of core.Transport interface
 func (p *Polybft) Multicast(msg *ibftProto.Message) {
-	p.ibftMsgMulticast(msg)
-
 	if err := p.consensusTopic.Publish(msg); err != nil {
 		p.logger.Warn("failed to multicast consensus message", "error", err)
 	}
