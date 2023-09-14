b526f1d58ca8a75340fd9f7aa6ac00955bbc2d1a
Stefan Negovanović <stefan@ethernal.tech>
2023-09-14 14:01:32 +0200

    Remove leftover governor-admin flag from docker script

--- docker/local/polygon-edge.sh
+++ docker/local/polygon-edge.sh
@@ -54,7 +54,6 @@ case "$1" in
                 --validators-prefix data- \
                 --reward-wallet 0xDEADBEEF:1000000 \
                 --native-token-config "Polygon:MATIC:18:true:$(echo "$secrets" | jq -r '.[0] | .address')" \
-                --governor-admin "$(echo "$secrets" | jq -r '.[0] | .address')" \
                 --bootnode "/dns4/node-1/tcp/1478/p2p/$(echo "$secrets" | jq -r '.[0] | .node_id')" \
                 --bootnode "/dns4/node-2/tcp/1478/p2p/$(echo "$secrets" | jq -r '.[1] | .node_id')" \
                 --bootnode "/dns4/node-3/tcp/1478/p2p/$(echo "$secrets" | jq -r '.[2] | .node_id')" \
