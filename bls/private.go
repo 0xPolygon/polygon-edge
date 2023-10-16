5de35ecda676aa5cea43d6812650400badcda80f
Stefan Negovanović <stefan@ethernal.tech>
2023-10-16 20:34:37 +0200

    Revert GeneratePrivateKey rename

--- bls/private.go
+++ bls/private.go
@@ -20,30 +20,6 @@ func NewZeroPrivateKey() *PrivateKey {
 	}
 }
 
-// PublicKey returns the public key from the PrivateKey
-func (p *PrivateKey) PublicKey() *PublicKey {
-	g2 := new(bn256.G2).ScalarMult(g2Point, p.s)
-
-	return &PublicKey{g2: g2}
-}
-
-// Marshal marshals private key to byte slice
-func (p *PrivateKey) Marshal() ([]byte, error) {
-	return []byte(strings.TrimPrefix(hex.EncodeBig(p.s), "0x")), nil
-}
-
-// Sign generates a simple BLS signature of the given message
-func (p *PrivateKey) Sign(message, domain []byte) (*Signature, error) {
-	point, err := hashToPoint(message, domain)
-	if err != nil {
-		return nil, err
-	}
-
-	g1 := new(bn256.G1).ScalarMult(point, p.s)
-
-	return &Signature{g1: g1}, nil
-}
-
 // UnmarshalPrivateKey unmarshals the private key from the given byte slice.
 // This function supports both raw big int string and hex-encoded big int string.
 func UnmarshalPrivateKey(data []byte) (*PrivateKey, error) {
@@ -63,4 +39,28 @@ func UnmarshalPrivateKey(data []byte) (*PrivateKey, error) {
 	}
 
 	return pk, nil
+}
+
+// PublicKey returns the public key from the PrivateKey
+func (p *PrivateKey) PublicKey() *PublicKey {
+	g2 := new(bn256.G2).ScalarMult(g2Point, p.s)
+
+	return &PublicKey{g2: g2}
+}
+
+// Marshal marshals private key hex (without 0x prefix) represented as a byte slice
+func (p *PrivateKey) Marshal() ([]byte, error) {
+	return []byte(strings.TrimPrefix(hex.EncodeBig(p.s), "0x")), nil
+}
+
+// Sign generates a simple BLS signature of the given message
+func (p *PrivateKey) Sign(message, domain []byte) (*Signature, error) {
+	point, err := hashToPoint(message, domain)
+	if err != nil {
+		return nil, err
+	}
+
+	g1 := new(bn256.G1).ScalarMult(point, p.s)
+
+	return &Signature{g1: g1}, nil
 }
--- bls/utils.go
+++ bls/utils.go
@@ -18,7 +18,7 @@ var (
 	g2Point = mustG2Point("198e9393920d483a7260bfb731fb5d25f1aa493335a9e71297e485b7aef312c21800deef121f1e76426a00665e5c4479674322d4f75edadd46debd5cd992f6ed090689d0585ff075ec9e99ad690c3395bc4b313370b38ef355acdadcd122975b12c85ea5db8c6deb4aab71808dcb408fe3d1e7690c43d37b4ce6cc0166fa7daa") //nolint
 )
 
-// GenerateBlsKey creates a random private and its corresponding public keys
+// GenerateBlsKey creates a random private key (and its corresponding public key)
 func GenerateBlsKey() (*PrivateKey, error) {
 	s, err := randomK(rand.Reader)
 	if err != nil {
