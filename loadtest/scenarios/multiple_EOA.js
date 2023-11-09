5a68091e309d5eaefb3ed4a92d2bf0e4d75efd85
Stefan Negovanović <stefan@ethernal.tech>
2023-11-09 11:44:04 +0100

    Set rate to 2000

--- loadtest/scenarios/multiple_EOA.js
+++ loadtest/scenarios/multiple_EOA.js
@@ -13,7 +13,7 @@ export const options = {
   scenarios: {
     constant_request_rate: {
       executor: 'constant-arrival-rate',
-      rate: 1500,
+      rate: 2000,
       timeUnit: '1s',
       duration: duration,
       preAllocatedVUs: 60,
